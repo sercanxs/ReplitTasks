45728b8994e54ee3320e0723c534b72d439678a4
Baldwin <sercanxssahin@hotmail.com>
2022-07-17 20:36:33 +0300

    Initial commit

---
+++ src/Statements/ConsoleMenu.java
@@ -0,0 +1,17 @@
+package Statements;
+
+public class ConsoleMenu {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("hi");
+	}
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module ReplitTasks {
+}
