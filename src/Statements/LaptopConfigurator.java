92cd8ead92085923b03c7e079a1c9a4e97d35f5f
Baldwin <sercanxssahin@hotmail.com>
2022-07-18 21:37:24 +0300

    1

--- src/Statements/LaptopConfigurator.java
+++ src/Statements/LaptopConfigurator.java
@@ -1,10 +1,12 @@
 package Statements;
 
+import java.util.Scanner;
+
 public class LaptopConfigurator {
 
 	public static void main(String[] args) {
 		
-		
+		Scanner sc = new Scanner(System.in);
 		
 		
 		
