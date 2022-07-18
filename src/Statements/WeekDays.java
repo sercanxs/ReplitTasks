e0cb6768679709b4a5df06456dceb929c35946c1
Baldwin <sercanxssahin@hotmail.com>
2022-07-18 21:26:26 +0300

    1

--- src/Statements/WeekDays.java
+++ src/Statements/WeekDays.java
@@ -15,6 +15,14 @@ public class WeekDays {
 	    
 	    //WRITE YOUR CODE HERE 
 	    
+	    if(day==1) {System.out.println("");}
+	    else if(day==2) {System.out.println("");}
+	    else if(day==3) {System.out.println("");}
+	    else if(day==4) {System.out.println("");}
+	    else if(day==5) {System.out.println("");}
+	    else if (day==6) {System.out.println("");}
+	    else if (day==7) {System.out.println("");}else {System.out.println("");}
+	    
 	    
 	
 	
