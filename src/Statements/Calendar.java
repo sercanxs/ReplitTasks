2514da1acb4841c6f98c63c53f277a8441627598
Baldwin <sercanxssahin@hotmail.com>
2022-07-18 21:15:13 +0300

    1

--- src/Statements/Calendar.java
+++ src/Statements/Calendar.java
@@ -5,7 +5,13 @@ public class Calendar {
 	public static void main(String[] args) {
 		
 		
-		
+		// DO NOT CHANGE
+	     Scanner scan = new Scanner (System.in);
+	     System.out.println("Enter month number");
+	     int monthNum = scan.nextInt();
+	     
+	       //WRITE YOUR CODE HERE:
+	       
 		
 		
 		
