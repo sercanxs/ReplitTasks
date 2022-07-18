01dc73acf10ac4dfa58bc93e942a93a0462f75e0
Baldwin <sercanxssahin@hotmail.com>
2022-07-18 21:29:11 +0300

    1

--- src/Statements/SwitchChar.java
+++ src/Statements/SwitchChar.java
@@ -1,5 +1,36 @@
 package Statements;
 
 public class SwitchChar {
+	
+	public static void main(String[] args) {
+		
+		 //DO NOT CHANGE
+	    Scanner scan = new Scanner(System.in);
+	    System.out.println("Enter command:");
+	    char response = scan.next().charAt(0);
+	    //WRITE YOUR CODE HERE 
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
 
 }
