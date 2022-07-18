0db1660ac9fb30bf7d8f1e4b963961ec7d5cb4f8
Baldwin <sercanxssahin@hotmail.com>
2022-07-18 21:06:19 +0300

    1

--- src/Statements/TernaryOperator2.java
+++ src/Statements/TernaryOperator2.java
@@ -6,7 +6,16 @@ public class TernaryOperator2 {
 		
 		
 		
-		
+		   //DO NOT TOUCH THESE LINES. TEACHER NEEDS THEM TO TEST YOUR CODE:
+	    // Variables are already declared and given
+	    Scanner scan = new Scanner(System.in);
+	    System.out.println("Enter number1:");
+			int num1 = scan.nextInt();
+			System.out.println("Enter number2:");
+			int num2 = scan.nextInt();
+			
+			//WRITE YOUR CODE HERE:
+			
 		
 		
 		
