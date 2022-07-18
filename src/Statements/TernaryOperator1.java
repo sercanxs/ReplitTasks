ceabfdbf659d40cb75d94606778da2d61f2db749
Baldwin <sercanxssahin@hotmail.com>
2022-07-18 21:04:39 +0300

    1

--- src/Statements/TernaryOperator1.java
+++ src/Statements/TernaryOperator1.java
@@ -1,5 +1,30 @@
 package Statements;
 
-public class TernaryOperator1 {
+import java.util.Scanner;
 
+public class TernaryOperator1 {
+	
+public static void main(String[] args) {
+
+	  
+	 //DO NOT TOUCH THESE LINES. TEACHER NEEDS THEM TO TEST YOUR CODE:
+	    // Variables are already declared and given
+	    
+	    Scanner scan = new Scanner(System.in);
+	    System.out.println("Enter number:");
+			int x = scan.nextInt();
+			//WRITE YOUT CODE HERE:
+			
+	    System.out.println(x>=5? x:-x);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
 }
