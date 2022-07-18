ef5bd171fa0ba70a8d378c3d8cf92cf69df6545d
Baldwin <sercanxssahin@hotmail.com>
2022-07-18 21:24:15 +0300

    1

---
+++ src/Statements/WeekDays.java
@@ -0,0 +1,27 @@
+package Statements;
+
+public class WeekDays {
+
+	public static void main(String[] args) {
+	
+	
+	
+		 // DO NOT CHANGE
+	     Scanner scan = new Scanner(System.in);
+	    System.out.println("Enter number:");
+	    int day = scan.nextInt();
+	    
+	    //WRITE YOUR CODE HERE 
+	    
+	    
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
