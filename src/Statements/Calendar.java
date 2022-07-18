db3163b5d196a9ecd85908450c666d78e844fd31
Baldwin <sercanxssahin@hotmail.com>
2022-07-18 21:19:40 +0300

    1

--- src/Statements/Calendar.java
+++ src/Statements/Calendar.java
@@ -14,18 +14,18 @@ public class Calendar {
 	     
 	       //WRITE YOUR CODE HERE:
 	       
-	     if(monthNum==1) {System.out.println("");}
-	     else if (monthNum==2) {System.out.println("");}
-	     else if (monthNum==3) {System.out.println("");}
-	     else if (monthNum==4) {System.out.println("");}
-	     else if (monthNum==5) {System.out.println("");}
-	     else if (monthNum==6) {System.out.println("");}
-	     else if (monthNum==7) {System.out.println("");}
-	     else if (monthNum==8) {System.out.println("");}
-	     else if (monthNum==9) {System.out.println("");}
-	     else if (monthNum==10) {System.out.println("");}
-	     else if (monthNum==11) {System.out.println("");}
-	     else if (monthNum==12) {System.out.println("");}
+	     if(monthNum==1) {System.out.println("January");}
+	     else if (monthNum==2) {System.out.println("February");}
+	     else if (monthNum==3) {System.out.println("March");}
+	     else if (monthNum==4) {System.out.println("April");}
+	     else if (monthNum==5) {System.out.println("May");}
+	     else if (monthNum==6) {System.out.println("June");}
+	     else if (monthNum==7) {System.out.println("July");}
+	     else if (monthNum==8) {System.out.println("August");}
+	     else if (monthNum==9) {System.out.println("September");}
+	     else if (monthNum==10) {System.out.println("October");}
+	     else if (monthNum==11) {System.out.println("November");}
+	     else if (monthNum==12) {System.out.println("December");}
 		
 		
 		
