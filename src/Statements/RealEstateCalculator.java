95a5f2279fb06f75df9682c5820a809359494521
Baldwin <sercanxssahin@hotmail.com>
2022-07-19 21:08:45 +0300

    1

--- src/Statements/RealEstateCalculator.java
+++ src/Statements/RealEstateCalculator.java
@@ -28,6 +28,7 @@ public static void main(String[] args) {
 		if(houseType.equals("Condo")) {
 			
 			System.out.println("Backyard is not available for condo!");
+			propertyPrice-=5000;
 		}
 		
 		
@@ -35,6 +36,25 @@ public static void main(String[] args) {
 		garage=scan.nextBoolean();
 		System.out.println("How many spots?");
 		garageSpots=scan.nextInt();
+		
+		if(garage) {
+			
+			propertyPrice+=20000*garageSpots;
+			
+			if(garageSpots>10) {
+				
+				System.out.println("Pardon, it's not a public parking!");
+				propertyPrice-=20000*garageSpots;
+			}
+					
+				
+			
+		}
+
+		
+		
+		
+		
 		System.out.println("How close is metro station?");
 		metroAccessibility=scan.nextFloat();
 		System.out.println("How close is highway?");
@@ -44,6 +64,62 @@ public static void main(String[] args) {
 		System.out.println("Does any of your family members smoke?");
 		smoking=scan.nextBoolean();
 		System.out.println("Market report has been generated.");
+		
+		
+		
+switch(houseType) {
+
+case "Condo":propertyPrice+=50000;
+break;
+case"Townhouse":propertyPrice+=75000;
+break;
+case"Single Family Home":propertyPrice+=95000;
+break;
+
+}
+
+propertyPrice+=numberOfBedrooms*30000;
+propertyPrice+=5000;
+
+
+if(metroAccessibility<=1) {
+	
+	propertyPrice+=10000;
+}else if(metroAccessibility<=3) {
+	propertyPrice+=5000;
+	
+}
+
+if(highwayAccessibility<=1) {
+	propertyPrice+=15000;
+}else if(highwayAccessibility<=5) {
+	propertyPrice+=8000;
+	
+}else if(highwayAccessibility<=20) {
+	propertyPrice+=4000;
+}
+
+if(schoolScore<=10 &schoolScore>=8) {
+	
+	propertyPrice+=45000;
+}else if(schoolScore>=4) {
+	
+	propertyPrice+=20000;
+}else {
+	
+	propertyPrice+=5000;
+}
+
+if(smoking) {
+	
+	propertyPrice-=5000;
+}
+	
+		
+		
+		
+		
+		
 		
 		
 		
