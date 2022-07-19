1b071abef4b9450c575d01d5ce94ed4d6b83077a
Baldwin <sercanxssahin@hotmail.com>
2022-07-19 16:55:54 +0300

    1

--- src/Statements/TipCalculator.java
+++ src/Statements/TipCalculator.java
@@ -2,4 +2,56 @@ package Statements;
 
 public class TipCalculator {
 
+	public static void main(String[] args) {
+		
+		System.out.println("Split:");
+		
+		
+		
+		System.out.println("Number of people:");
+		
+		
+		System.out.println("Check amount:");
+		
+		System.out.println("Service Quality:");
+		
+		System.out.println("");
+		
+		System.out.println("");
+		
+		System.out.println("");
+		
+		System.out.println("");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
