f92a1ef31726deef95c8a30e81ac7a48f98e86f7
Baldwin <sercanxssahin@hotmail.com>
2022-07-19 16:45:32 +0300

    1

--- src/Statements/HTTPStatusCodes.java
+++ src/Statements/HTTPStatusCodes.java
@@ -1,5 +1,52 @@
 package Statements;
 
+import java.util.Scanner;
+
 public class HTTPStatusCodes {
 
+	public static void main(String[] args) {
+		
+		
+		//Do Not Change
+	    System.out.println("Enter status code:");
+	    Scanner scan=new Scanner (System.in);
+	    int status= scan.nextInt();
+	    
+	      //WRITE YOUR CODE HERE:
+	      
+	    
+	    
+	
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	}
+	  
+	
+	
+	
+	
+	
+	
+	
+	
 }
