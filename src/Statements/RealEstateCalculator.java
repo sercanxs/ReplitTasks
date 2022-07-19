28362f855008ecb27c14ef65194b26220f88133d
Baldwin <sercanxssahin@hotmail.com>
2022-07-19 20:29:02 +0300

    1

--- src/Statements/RealEstateCalculator.java
+++ src/Statements/RealEstateCalculator.java
@@ -1,7 +1,59 @@
 package Statements;
 
+import java.util.Scanner;
+
 public class RealEstateCalculator {
 public static void main(String[] args) {
+	
+	
+	 //DO NOT CHANGE
+    int propertyPrice = 0; 
+		int numberOfBedrooms, garageSpots;
+		float metroAccessibility, schoolScore, highwayAccessibility;
+		boolean backyard, smoking, garage;
+		String houseType;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("*****************************************");
+		System.out.println("* Welcome to the RealEstate calculator! *");
+		System.out.println("*****************************************");
+    //WRITE YOUR CODE HERE
+
+		System.out.println("Enter your property type:");
+		System.out.println("How many bedrooms do you have?");
+		System.out.println("Do you have a backyard?");
+		backyard=scan.nextBoolean();
+		
+		
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
 }
