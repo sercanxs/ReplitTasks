91d8887e4386c885886d3258dff4be62f44f9a00
Baldwin <sercanxssahin@hotmail.com>
2022-07-19 17:35:31 +0300

    1

---
+++ src/Statements/BurgerOrChicken.java
@@ -0,0 +1,56 @@
+package Statements;
+
+import java.util.Scanner;
+
+public class BurgerOrChicken {
+	public static void main(String[] args) {
+		
+		Scanner sc =new Scanner(System.in);
+		
+		String order =sc.nextLine();
+		
+		if(order.equals("burger")||order.equals("chicken")){
+			
+			System.out.println("10");
+			
+			
+			
+		}else if(order.equals("soda")) {
+			
+			
+			System.out.println(2.0);
+		}else if(order.equals("fries")) {
+			
+			System.out.println(3.5);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+
+}
