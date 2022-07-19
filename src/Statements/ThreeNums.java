fa299807b068705dd54c70c95a36b72ad7003c6e
Baldwin <sercanxssahin@hotmail.com>
2022-07-19 20:18:34 +0300

    1

--- src/Statements/ThreeNums.java
+++ src/Statements/ThreeNums.java
@@ -1,7 +1,62 @@
 package Statements;
 
+import java.util.Scanner;
+
 public class ThreeNums {
 public static void main(String[] args) {
+	
+    //DO NOT TOUCH THESE LINES. TEACHER NEEDS THEM TO TEST YOUR CODE:
+   // Variables are already declared and given
+   Scanner s = new Scanner(System.in);
+   
+   int n1 = s.nextInt();
+   int n2 = s.nextInt();
+   int n3 = s.nextInt();
+   
+   //your code here
+
+ 
+if(n1>n2&n1>n3) {
+	System.out.println("n1 is bigger");
+	
+}else if(n2>n3) {
+	System.out.println("n2 is bigger");
+}else {
+System.out.println("n3 is bigger");	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
 }
