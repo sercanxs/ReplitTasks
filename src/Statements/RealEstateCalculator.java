14e7e370537bed27a2330bbe8d59c4dd2d9ba314
Baldwin <sercanxssahin@hotmail.com>
2022-07-19 20:38:04 +0300

    1

--- src/Statements/RealEstateCalculator.java
+++ src/Statements/RealEstateCalculator.java
@@ -25,15 +25,33 @@ public static void main(String[] args) {
 		
 		System.out.println("Do you have a backyard?");
 		backyard=scan.nextBoolean();
+		if(houseType.equals("Condo")) {
+			
+			System.out.println("Backyard is not available for condo!");
+		}
+		
+		
+		System.out.println("Do you have garage?");
+		garage=scan.nextBoolean();
+		System.out.println("How many spots?");
+		garageSpots=scan.nextInt();
+		System.out.println("How close is metro station?");
+		metroAccessibility=scan.nextFloat();
+		System.out.println("How close is highway?");
+		highwayAccessibility=scan.nextFloat();
+		System.out.println("What's the rating of nearest school?");
+		schoolScore=scan.nextFloat();
+		System.out.println("Does any of your family members smoke?");
+		smoking=scan.nextBoolean();
+		System.out.println("Market report has been generated.");
 		
 		
 		
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
+		
+		
+		
+		
+		System.out.println("Your estimate market price is: "+ propertyPrice+"$");
 	
 	
 	
