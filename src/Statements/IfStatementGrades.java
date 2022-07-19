19f78716d28752eaa13d0221d3ba7ce393236fa9
Baldwin <sercanxssahin@hotmail.com>
2022-07-19 17:39:45 +0300

    1

---
+++ src/Statements/IfStatementGrades.java
@@ -0,0 +1,52 @@
+package Statements;
+
+import java.util.Scanner;
+
+public class IfStatementGrades {
+	public static void main(String[] args) {
+		
+		   //DO NOT TOUCH THESE LINES. TEACHER NEEDS THEM TO TEST YOUR CODE:
+	    // Variables are already declared and given
+	    Scanner s = new Scanner(System.in);
+	    
+	    int grade = s.nextInt();
+	    
+	    //Write your code here:
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
