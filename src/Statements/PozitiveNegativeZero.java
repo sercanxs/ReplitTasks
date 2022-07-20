ff84a7f47ee40fe0c097263f3dc956def06f9a8f
Baldwin <sercanxssahin@hotmail.com>
2022-07-20 16:44:35 +0300

    1

--- src/Statements/PozitiveNegativeZero.java
+++ src/Statements/PozitiveNegativeZero.java
@@ -4,7 +4,13 @@ public class PozitiveNegativeZero {
 
 	public static void main(String[] args) {
 		
-		
+		  //DO NOT TOUCH THESE LINES. TEACHER NEEDS THEM TO TEST YOUR CODE:
+	    // Variables are already declared and given
+	    Scanner s = new Scanner(System.in);
+	    int num = s.nextInt();
+
+	    //your code here
+	    
 		
 		
 		
