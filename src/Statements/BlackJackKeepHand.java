0c38ca0e57d2b3297e60835baba93759b517df22
Baldwin <sercanxssahin@hotmail.com>
2022-07-20 15:43:52 +0300

    1

--- src/Statements/BlackJackKeepHand.java
+++ src/Statements/BlackJackKeepHand.java
@@ -1,8 +1,45 @@
 package Statements;
 
+import java.util.Scanner;
+
 public class BlackJackKeepHand {
 
 	public static void main(String[] args) {
+		
+		  //DO NOT TOUCH THESE LINES. TEACHER NEEDS THEM TO TEST YOUR CODE:
+	    // Variables are already declared and given
+	    Scanner s = new Scanner(System.in);
+	    
+	    int house = s.nextInt();
+	    int player = s.nextInt();
+	    
+	    //Write your code here:
+	    
+	if(player>21) {System.out.println("player bust");}    
+   else if(player>house) {System.out.println("player win");}
+   else if(player==house) {System.out.println("its a tie");}
+   else if(player<house) {System.out.println("player loss ");}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 }
