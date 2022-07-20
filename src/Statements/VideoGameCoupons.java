3d8bdd80f881471338d9315496a42900554b517a
Baldwin <sercanxssahin@hotmail.com>
2022-07-20 16:43:06 +0300

    1

--- src/Statements/VideoGameCoupons.java
+++ src/Statements/VideoGameCoupons.java
@@ -1,8 +1,30 @@
 package Statements;
 
+import java.util.Scanner;
+
 public class VideoGameCoupons {
 
 	public static void main(String[] args) {
+		
+		
+		System.out.println("Enter number of coupons:");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a =sc.nextInt();
+		
+		int candies = a/10;
+		
+		int gumballs=a%10/3;
+		
+		if(a>2) {
+			System.out.println("Number of Candies: "+candies);
+		System.out.println("Number of Gumballs: "+gumballs);	
+		}else {
+			System.out.println("Not enough coupons");
+		}
+		
+		
 		
 	}
 	
