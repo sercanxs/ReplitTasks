5b29cec2c165ea1a351b37b0ea373d5a67144b89
Baldwin <sercanxssahin@hotmail.com>
2022-07-20 15:31:34 +0300

    1

--- src/Statements/HappyValleySchoolSystem.java
+++ src/Statements/HappyValleySchoolSystem.java
@@ -1,8 +1,48 @@
 package Statements;
 
+import java.util.Scanner;
+
 public class HappyValleySchoolSystem {
 
 	public static void main(String[] args) {
+		
+	    //DO NOT TOUCH THESE LINES. TEACHER NEEDS THEM TO TEST YOUR CODE:
+	    // Variables are already declared and given
+
+	    Scanner input = new Scanner(System.in);
+	    
+	     System.out.println("Enter age:");
+	    int age = input.nextInt();
+	    
+	    
+	    //WRITE YOUR CODE HERE 
+	    
+	    
+	    if(age<2) {System.out.println("ineligible");}
+	    else if(age==2) {System.out.println("toddler");}
+	    else if(age<6) {System.out.println("early childhood");}
+	    else if(age<8) {System.out.println("young reader");}
+	    else if(age<11) {System.out.println("elementary");}
+	    else if(age<13) {System.out.println("middle");}
+	    else if(age==13) {System.out.println("impossible");}
+	    else if(age<17) {System.out.println("high school");}
+	    else if(age<19) {System.out.println("scholar");}
+	    else {System.out.println("ineligible");}
+	    
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 	
