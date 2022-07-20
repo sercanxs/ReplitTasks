e3b6d94f19a1f22c423d786a536a6425f6d67ac0
Baldwin <sercanxssahin@hotmail.com>
2022-07-20 15:53:21 +0300

    1

--- src/Statements/FindMidNumber.java
+++ src/Statements/FindMidNumber.java
@@ -1,8 +1,56 @@
 package Statements;
 
+import java.util.Scanner;
+
 public class FindMidNumber {
 
 	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter first number:");
+		
+		int first = sc.nextInt();
+		
+		System.out.println("Enter second number:");
+		
+		int second = sc.nextInt();
+		
+		System.out.println("Enter third number:");
+		
+		int third = sc.nextInt();
+		
+		if(first>second&second>third) {
+			System.out.println("Medium value is: "+second);
+		}else if(second>first&third>second) {
+			System.out.println("Medium value is: "+second);
+		}else if(third>first&first>second) {
+			System.out.println("Medium value is: "+first);
+		}else if(second>first&first>third) {
+			System.out.println("Medium value is: "+first);
+		}else if(first>third&third>second) {
+			System.out.println("Medium value is: "+third);
+		}else if(first<third&third<second) {
+			System.out.println("Medium value is: "+third);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 	
