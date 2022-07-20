8798ac25efe99f369af0590a83272f83e61f1983
Baldwin <sercanxssahin@hotmail.com>
2022-07-20 15:54:50 +0300

    1

--- src/Statements/ApartmentLeasingI.java
+++ src/Statements/ApartmentLeasingI.java
@@ -2,4 +2,29 @@ package Statements;
 
 public class ApartmentLeasingI {
 
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
 }
