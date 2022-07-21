534bef1967e470934244c75e7eb392418ca5816d
Baldwin <sercanxssahin@hotmail.com>
2022-07-21 19:01:38 +0300

    1

--- src/String/CarInsuranceQuote.java
+++ src/String/CarInsuranceQuote.java
@@ -1,9 +1,26 @@
 package String;
 
+import java.util.Scanner;
+
 public class CarInsuranceQuote {
 public static void main(String[] args) {
 	
-	
+    //DO NOT CHANGE
+    double premium = 0;
+		int accidentsAmount = 0;
+		int daysDrivenToWorkOrSchool = 0;
+		int milesToWorkOrSchool = 0;
+		String vehicleOwnership = "";
+		String vehicleUsage = "";
+		String continuousInsurance = "";
+		String education = "";
+		String name = "";
+		String referenceNumber = "";
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Welcome to the CountyFarm car insurance!");
+    //WRITE YOUR CODE HERE
+
+
 	
 	
 	
