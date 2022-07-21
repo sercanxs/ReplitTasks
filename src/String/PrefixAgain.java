0885266171a406df0d5b56be608f4b64503e81d4
Baldwin <sercanxssahin@hotmail.com>
2022-07-21 15:22:43 +0300

    1

--- src/String/PrefixAgain.java
+++ src/String/PrefixAgain.java
@@ -1,10 +1,33 @@
 package String;
 
+import java.util.Scanner;
+
 public class PrefixAgain {
 public static void main(String[] args) {
 	
+	  Scanner scan = new Scanner(System.in);
+	    String str = scan.next();
+	    int n = scan.nextInt();
+
+
+if(str.isEmpty()) {
+	System.out.println("Str is empty");
+	return;
+	
+}
+if(n>str.length()) {
+	
+	System.out.println("n is out of the range");
+	return;
+}	
+
+System.out.println(str.substring(0,1));
+if(str.contains(str.substring(0,1))) {
 	
 	
+}
+
+
 	
 	
 	
