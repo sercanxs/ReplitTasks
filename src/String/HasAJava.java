33989afdc4a1e05b37cdcdd459e0c76b4f5831ab
Baldwin <sercanxssahin@hotmail.com>
2022-07-21 16:27:37 +0300

    1

--- src/String/HasAJava.java
+++ src/String/HasAJava.java
@@ -1,11 +1,30 @@
 package String;
 
+import java.util.Scanner;
+
 public class HasAJava {
 public static void main(String[] args) {
+	  
 	
 	
 	
+	boolean exists = false;
+	    Scanner scan = new Scanner(System.in);
+	    String word = scan.next();
+ if(word.length()<5) {
+	 if(word.substring(0,word.length()).contains("java")) {
+		 exists=true;
+		 
+	 }
+	 
+	 
+ }
+ else if(word.substring(0,5).contains("java")) {
+	    	
+	    	exists=true;
+	    }
 	
+	System.out.println(exists);
 	
 	
 	
