ef59965e5a2231c3212f7261876cb6d8f8a9fa92
Baldwin <sercanxssahin@hotmail.com>
2022-07-21 15:24:18 +0300

    1

--- src/String/GetSandwich.java
+++ src/String/GetSandwich.java
@@ -1,5 +1,38 @@
 package String;
 
 public class GetSandwich {
-
+public static void main(String[] args) {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+	
+	
+	
+	
 }
