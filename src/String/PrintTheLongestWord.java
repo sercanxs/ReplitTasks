ae5051930a5b33ee9629183f0aa693536dfc6d5b
Baldwin <sercanxssahin@hotmail.com>
2022-07-22 19:02:16 +0300

    1

--- src/String/PrintTheLongestWord.java
+++ src/String/PrintTheLongestWord.java
@@ -1,7 +1,22 @@
 package String;
 
+import java.util.Scanner;
+
 public class PrintTheLongestWord {
 public static void main(String[] args) {
+	
+    //DO NOT CHANGE
+    Scanner scan = new Scanner(System.in);
+    String word1 = scan.next();
+    String word2 = scan.next();
+    //WIRTE YOUR CODE HERE
+    
+    if(word1.length()>word2.length()) {
+    	
+    	System.out.println(word1);
+    	
+    }else {System.out.println(word2);}
+	
 	
 	
 	
