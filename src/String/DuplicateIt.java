54cfa01fda8e1414b8d4aa601784fea612d1943d
Baldwin <sercanxssahin@hotmail.com>
2022-07-22 19:34:05 +0300

    1

--- src/String/DuplicateIt.java
+++ src/String/DuplicateIt.java
@@ -1,11 +1,19 @@
 package String;
 
+import java.util.Scanner;
+
 public class DuplicateIt {
 public static void main(String[] args) {
+	 Scanner scan = new Scanner(System.in);
+	    String word1 = scan.next();
+	    String word2 = scan.next();
+	    //YOUR CODE HERE
+	    
 	
 	
+	word1=word1.concat(word2).concat(word2).concat(word1);
 	
-	
+	System.out.println(word1);
 	
 	
 	
