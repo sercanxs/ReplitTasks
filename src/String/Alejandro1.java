fc570473fc6e4821242124b250f9e6f25feac285
Baldwin <sercanxssahin@hotmail.com>
2022-07-22 17:20:46 +0300

    1

--- src/String/Alejandro1.java
+++ src/String/Alejandro1.java
@@ -1,9 +1,19 @@
 package String;
 
+import java.util.Scanner;
+
 public class Alejandro1 {
 public static void main(String[] args) {
+	   Scanner s = new Scanner(System.in);
+	    String a = s.nextLine();
+
+	    //your code here
+if(a.toLowerCase().contains("alejandro")) {
+	System.out.println("read");
 	
 	
+}else {System.out.println("don't read");}
 	
-}
+	
+ }
 }
