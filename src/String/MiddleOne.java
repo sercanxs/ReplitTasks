9db8f3b290bd3f63e3ebc8767719d44ce6d5b44d
Baldwin <sercanxssahin@hotmail.com>
2022-07-22 19:42:40 +0300

    1

--- src/String/MiddleOne.java
+++ src/String/MiddleOne.java
@@ -1,9 +1,29 @@
 package String;
 
+import java.util.Scanner;
+
 public class MiddleOne {
 public static void main(String[] args) {
 	
-	
+	   Scanner scan = new Scanner(System.in);
+	    String word = scan.next();
+	    //YOUR CODE HERE
+	    
+	    
+	    
+	    if(word.length()%2==1) {
+	    	if(word.length()==1) {System.out.println(word+word+word);}
+	    	else {         System.out.println(word.substring(word.length()/2,word.length()/2+1));        }
+	    	
+	    	
+	    	
+	    }else {if(word.length()==2) {System.out.println(word+word);}else { System.out.println(word.substring(word.length()/2-1,word.length()/2+1)  );                      }
+	    	
+	    	
+	    	
+	    	
+	    	
+	    }
 	
 	
 	
