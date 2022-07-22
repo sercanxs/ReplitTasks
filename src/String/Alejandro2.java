6327953205d47bcedd22974acc15852bd8312a07
Baldwin <sercanxssahin@hotmail.com>
2022-07-22 17:25:16 +0300

    1

--- src/String/Alejandro2.java
+++ src/String/Alejandro2.java
@@ -1,8 +1,23 @@
 package String;
 
+import java.util.Scanner;
+
 public class Alejandro2 {
 public static void main(String[] args) {
-	
+	 Scanner s = new Scanner(System.in);
+	    String a = s.nextLine();
+
+	    //your code here
+	    
+	    if(a.contains("alejandro")&a.contains("project")){
+	    	
+	    	System.out.println("read");
+	    	
+	    	
+	    }else {
+	    	
+	    	System.out.println("don't read");
+	    }
 	
 	
 	
