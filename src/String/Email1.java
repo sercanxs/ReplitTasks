fe702da8c7d71b671487d7030a44ed45c64dfef4
Baldwin <sercanxssahin@hotmail.com>
2022-07-22 16:36:34 +0300

    1

--- src/String/Email1.java
+++ src/String/Email1.java
@@ -1,12 +1,30 @@
 package String;
 
+import java.util.Scanner;
+
 public class Email1 {
 	public static void main(String[] args) {
 		
-		
-		
-		
-		
+	    Scanner scan = new Scanner(System.in);
+	    String email = scan.next();
+	    
+	    String s ="";
+	    String s2="";
+	    
+		if(email.contains("_")) {
+			
+			
+			
+			s=email.substring(0,email.indexOf("_"));
+			s2=email.substring(email.indexOf("_")+1,email.indexOf("@"));
+			
+			email=email.replace(s, s2);
+			email=s2+email.substring(email.indexOf("_"),email.length()).replace(s2, s);
+			
+			
+			
+		}
+		System.out.println(email);
 		
 		
 		
