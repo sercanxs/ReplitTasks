a43eaac5415223442fe983306d4265550fe69a9e
Baldwin <sercanxssahin@hotmail.com>
2022-07-22 19:48:48 +0300

    1

--- src/String/PrintFirstCharacter.java
+++ src/String/PrintFirstCharacter.java
@@ -1,10 +1,17 @@
 package String;
 
+import java.util.Scanner;
+
 public class PrintFirstCharacter {
 public static void main(String[] args) {
 	
 	
-	
+	  //DO NOT CHANGE
+    Scanner scan = new Scanner(System.in);
+    String word = scan.next();
+    //WRITE YOUR CODE HERE:
+    
+    System.out.println(word.substring(0,1));
 	
 	
 	
