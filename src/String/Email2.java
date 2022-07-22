e002747dc5af471f97e2cb46fb2d242004246c0c
Baldwin <sercanxssahin@hotmail.com>
2022-07-22 16:45:52 +0300

    1

--- src/String/Email2.java
+++ src/String/Email2.java
@@ -1,10 +1,33 @@
 package String;
 
+import java.util.Scanner;
+
 public class Email2 {
 public static void main(String[] args) {
+	   Scanner scan = new Scanner(System.in);
+	    String email = scan.next();
+	    
+	    
+	    email=email.substring(0,1).toUpperCase()+email.substring(1,email.length());
 	
 	
 	
-	
+	    
+	    String firstName=email.substring(0,email.indexOf("_"));
+	    String SecondName=email.substring(email.indexOf("_")+1,email.indexOf("@"));
+	    String domain = email.substring(email.indexOf("@")+1,email.indexOf("."));
+	    
+	    
+	    SecondName=SecondName.substring(0,1).toUpperCase()+SecondName.substring(1,SecondName.length());
+	    
+	    
+	    System.out.println("First name: "+firstName);
+	    System.out.println("Last name: "+SecondName);
+	    System.out.println("Domain: "+domain);
+	    
+	    
+	    
+	    
+	    
 }
 }
