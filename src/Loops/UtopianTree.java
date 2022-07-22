6e88f0adb44f0a1903db95b8e466d70587a29e15
Baldwin <sercanxssahin@hotmail.com>
2022-07-22 20:33:19 +0300

    1

--- src/Loops/UtopianTree.java
+++ src/Loops/UtopianTree.java
@@ -5,6 +5,18 @@ public static void main(String[] args) {
 	
 	
 	
+	for(int i=1;i<4;i++) {
+		System.out.println("year "+i+" - growth "+1+" cm");
+		System.out.println("tree size: "+i+"cm");
+		
+	}
+	
+	
+	for(int i=4;i<11;i++) {
+		System.out.println("year "+i+" - growth "+2+" cm");
+		System.out.println("tree size: "+((i-3)*2+3)+"cm");
+		
+	}
 	
 	
 	
