b159ad595e181712e9b206d42f59f9e9b7725660
Baldwin <sercanxssahin@hotmail.com>
2022-07-22 20:22:19 +0300

    1

--- src/String/VerifyContains.java
+++ src/String/VerifyContains.java
@@ -1,14 +1,19 @@
 package String;
 
+import java.util.Scanner;
+
 public class VerifyContains {
 public static void main(String[] args) {
 	
+	   Scanner scan = new Scanner(System.in);
+	    String word = scan.nextLine();
+	    String sentence = scan.nextLine();
+	    //WRITE YOUR CODE HERE
 	
 	
 	
 	
-	
-	
+	System.out.println(sentence.contains(word));
 	
 	
 	
