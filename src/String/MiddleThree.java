f7d61b4a31881beadb3fa9d5bfb9b3597a16803e
Baldwin <sercanxssahin@hotmail.com>
2022-07-22 17:26:21 +0300

    1

--- src/String/MiddleThree.java
+++ src/String/MiddleThree.java
@@ -1,5 +1,40 @@
 package String;
 
 public class MiddleThree {
-
+public static void main(String[] args) {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
