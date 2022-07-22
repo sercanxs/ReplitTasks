0e8ae11788a286d2368f323534028caf537ee259
Baldwin <sercanxssahin@hotmail.com>
2022-07-22 20:37:36 +0300

    1

--- src/Loops/RepeatSeperator.java
+++ src/Loops/RepeatSeperator.java
@@ -1,12 +1,24 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class RepeatSeperator {
 public static void main(String[] args) {
 	
+    Scanner scan = new Scanner(System.in);
+    String word = scan.next();
+    String separator = scan.next();
+    int count = scan.nextInt();
+    
 	
-	
-	
-	
+	for(int i=1;i<count;i++) {
+		
+		
+		System.out.print(word);
+		System.out.print(separator);
+		
+	}
+	System.out.println(word);
 	
 	
 	
