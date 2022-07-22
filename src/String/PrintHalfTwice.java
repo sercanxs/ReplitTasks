ea4db7f9f5f238e333aea8022b92f12cab0619e4
Baldwin <sercanxssahin@hotmail.com>
2022-07-22 19:06:32 +0300

    1

--- src/String/PrintHalfTwice.java
+++ src/String/PrintHalfTwice.java
@@ -1,10 +1,16 @@
 package String;
 
+import java.util.Scanner;
+
 public class PrintHalfTwice {
 
 	public static void main(String[] args) {
 		
-		
+		Scanner scan = new Scanner(System.in);
+	    String word = scan.next();
+	    //WRITE YOUR CODE HERE
+	    
+	    System.out.println(word.substring(0,word.length()/2)+word.substring(0,word.length()/2));
 		
 		
 		
