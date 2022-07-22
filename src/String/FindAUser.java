c230d2eb7a280f145dc790c29686e293bd0d5d49
Baldwin <sercanxssahin@hotmail.com>
2022-07-22 20:17:40 +0300

    1

--- src/String/FindAUser.java
+++ src/String/FindAUser.java
@@ -1,15 +1,22 @@
 package String;
 
+import java.util.Scanner;
+
 public class FindAUser {
 public static void main(String[] args) {
 	
 	
 	
+    Scanner scan = new Scanner(System.in);
+    //WRITE YOUR CODE HERE 
+
+    System.out.println("Enter full name:");
+    String word = scan.nextLine();
+    
+    
 	
 	
-	
-	
-	
+	if(word.equalsIgnoreCase("Max Payne")|word.equalsIgnoreCase("Alan Wake")) {System.out.println("User found!");}else {System.out.println("User not found!");}
 	
 	
 	
