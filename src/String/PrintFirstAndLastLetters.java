17ecce19ca85121704843faae032231991f658b0
Baldwin <sercanxssahin@hotmail.com>
2022-07-22 19:44:54 +0300

    1

--- src/String/PrintFirstAndLastLetters.java
+++ src/String/PrintFirstAndLastLetters.java
@@ -1,14 +1,19 @@
 package String;
 
+import java.util.Scanner;
+
 public class PrintFirstAndLastLetters {
 public static void main(String[] args) {
 	
+    //DO NOT CHANGE
+    Scanner scan = new Scanner(System.in);
+    String word = scan.next();
+    //WRITE YOUR CODE HERE
 	
 	
 	
 	
-	
-	
+	System.out.println(word.substring(0,1)+word.substring(word.length()-1,word.length()));
 	
 	
 	
