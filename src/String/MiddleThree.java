a4f787a5d6669288958687effb8a6ee4a84d6032
Baldwin <sercanxssahin@hotmail.com>
2022-07-22 17:35:18 +0300

    1

--- src/String/MiddleThree.java
+++ src/String/MiddleThree.java
@@ -1,8 +1,22 @@
 package String;
 
+import java.util.Scanner;
+
 public class MiddleThree {
 public static void main(String[] args) {
-	
+	 Scanner scan = new Scanner(System.in);
+	    String word = scan.next();
+	    //YOUR CODE HERE
+	    
+	    if(word.length()>4) {
+	    	
+	    	if(word.length()%2==1)
+	    	 {       System.out.println(word.substring(word.length()/2-1,word.length()/2+2));                           }
+	    	else {System.out.println("invalid");}
+	    	
+	    	
+	    	
+	    }else {System.out.println("invalid");}
 	
 	
 	
