3149c4f64ec51550d47a2996b749c76902945c27
Baldwin <sercanxssahin@hotmail.com>
2022-07-22 19:51:52 +0300

    1

--- src/String/FindTheLength.java
+++ src/String/FindTheLength.java
@@ -1,10 +1,18 @@
 package String;
 
+import java.util.Scanner;
+
 public class FindTheLength {
 public static void main(String[] args) {
 	
 	
+	System.out.println("Please enter the text:");
 	
+	Scanner sc = new Scanner(System.in);
+	
+	String word = sc.nextLine();
+	
+	System.out.println("Length is: "+word.length());
 	
 	
 	
