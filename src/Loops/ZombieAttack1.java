6b18f1a9f84446ac5e472e6ba4ef8cab0f40ed09
Baldwin <sercanxssahin@hotmail.com>
2022-07-24 21:01:22 +0300

    1

--- src/Loops/ZombieAttack1.java
+++ src/Loops/ZombieAttack1.java
@@ -1,13 +1,29 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class ZombieAttack1 {
 public static void main(String[] args) {
 	
 	
+    Scanner scan = new Scanner(System.in);
+    int inhabitants = scan.nextInt();
+    int day=0;
 	
 	
-	
-	
+	for(int i=inhabitants;i>=0;i=i/2) {
+		
+		
+		if(i==0) {
+			System.out.println("---- EXTINCT ----");
+			break;
+		}
+		
+		
+		System.out.println("Day "+day+" ["+i+"]");
+		
+day++;
+	}
 	
 	
 	
