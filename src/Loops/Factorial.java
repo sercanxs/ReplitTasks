ddfc62e0bbeb0a7faa91c397e5cb484e564bb50a
Baldwin <sercanxssahin@hotmail.com>
2022-07-24 20:31:53 +0300

    1

--- src/Loops/Factorial.java
+++ src/Loops/Factorial.java
@@ -1,18 +1,27 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class Factorial {
 public static void main(String[] args) {
 	
 	
+    Scanner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+    int f=1;
+
 	
 	
 	
+	for(n=n;n>0;n--) {
+		
+	f=f*n;	
+		
+	}
 	
 	
 	
-	
-	
-	
+	System.out.println(f);
 	
 	
 	
