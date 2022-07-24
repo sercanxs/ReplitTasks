6a57a2a3da0d40f2064f11306ca6afc8b841fb4f
Baldwin <sercanxssahin@hotmail.com>
2022-07-24 20:16:26 +0300

    1

--- src/Loops/CountHi.java
+++ src/Loops/CountHi.java
@@ -1,5 +1,40 @@
 package Loops;
 
 public class CountHi {
-
+public static void main(String[] args) {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
 }
