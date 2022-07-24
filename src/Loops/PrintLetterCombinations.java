851d72c110b98dca0dbfd59dfef6635f893bb3f6
Baldwin <sercanxssahin@hotmail.com>
2022-07-24 20:44:51 +0300

    1

--- src/Loops/PrintLetterCombinations.java
+++ src/Loops/PrintLetterCombinations.java
@@ -3,6 +3,48 @@ package Loops;
 public class PrintLetterCombinations {
 public static void main(String[] args) {
 	
+	String b="";
+	String c="";
+	
+	for(int i=1;i<=5;i++) {
+	
+		switch(i) {
+		case 1: b="z";
+			break;
+		case 2: b="y";
+			break;
+		case 3: b="x";
+		break;
+		case 4: b="w";
+			break;
+		case 5: b="v";
+			break;
+		
+		
+		}
+		
+	for(int a=1;a<=5;a++) {
+		
+		switch(a) {
+		case 1: System.out.println(b+"z");
+			break;
+		case 2: System.out.println(b+"y");
+			break;
+		case 3: System.out.println(b+"x");
+		break;
+		case 4: System.out.println(b+"w");
+			break;
+		case 5: System.out.println(b+"v");
+			break;
+		
+		
+		
+	}
+		
+	
+		
+		
+	}
 	
 	
 	
@@ -11,9 +53,6 @@ public static void main(String[] args) {
 	
 	
 	
-	
-	
-	
-	
+}
 }
 }
