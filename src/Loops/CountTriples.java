46a8630509a2e610cd9cddd68e5641b8ff3b6eb8
Baldwin <sercanxssahin@hotmail.com>
2022-07-24 17:34:17 +0300

    1

--- src/Loops/CountTriples.java
+++ src/Loops/CountTriples.java
@@ -1,17 +1,35 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class CountTriples {
 public static void main(String[] args) {
 	
 	
 	
+    Scanner scan = new Scanner(System.in);
+    String str = scan.next();
+    int count = 0 ;
+    //WRITE YOUR CODE HERE
 	
 	
+	for(int i =0;i<=str.length()-3;i++) {
+		
+		if(str.charAt(i)==str.charAt(i+1)){
+			if(str.charAt(i+1)==str.charAt(i+2)) {
+				
+				count++;
+			}
+			
+			
+			
+		}
+		
+		
+		
+	}
 	
-	
-	
-	
-	
+	System.out.println(count);
 	
 	
 	
