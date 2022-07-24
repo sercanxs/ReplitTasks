7a3c24a8cc414130c46a1fd962e9772037a221eb
Baldwin <sercanxssahin@hotmail.com>
2022-07-24 16:40:27 +0300

    1

--- src/Loops/PrintEvenfrom80to20.java
+++ src/Loops/PrintEvenfrom80to20.java
@@ -1,5 +1,7 @@
 package Loops;
 
+import java.util.Iterator;
+
 public class PrintEvenfrom80to20 {
 public static void main(String[] args) {
 	
@@ -7,10 +9,16 @@ public static void main(String[] args) {
 	
 	
 	
+for(int i =80;i>19;i-=2) {
 	
+	System.out.print(i);
 	
+	if(i!=20) {
+		
+		System.out.print(" ");
+	}
 	
-	
+}
 	
 	
 	
