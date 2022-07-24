33073024c47f93843f38646348202ed1d1ddf151
Baldwin <sercanxssahin@hotmail.com>
2022-07-24 16:35:12 +0300

    1

--- src/Loops/PrintLetters.java
+++ src/Loops/PrintLetters.java
@@ -1,10 +1,29 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class PrintLetters {
 public static void main(String[] args) {
 	
 	
-	
+    Scanner scan = new Scanner(System.in);
+    char start = scan.next().charAt(0);
+    char end = scan.next().charAt(0);
+    
+    int a = start;
+    int b = end;
+    
+    
+    
+    if(a>b) {return;}
+    
+    for(int i=a;i<=b;i++) {
+    	
+    	System.out.print((char)i);
+    	
+    	
+    	
+    }
 	
 	
 	
