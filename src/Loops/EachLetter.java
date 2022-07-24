09d41d6f0a96e9faa0670725d49172521d5c2758
Baldwin <sercanxssahin@hotmail.com>
2022-07-24 17:27:28 +0300

    1

--- src/Loops/EachLetter.java
+++ src/Loops/EachLetter.java
@@ -1,14 +1,27 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class EachLetter {
 public static void main(String[] args) {
 	
 	
+    Scanner inp = new Scanner(System.in);
+    String word = inp.nextLine();
+    //write your code below
+    
 	
 	
 	
-	
-	
+	for(int i=0;i<=word.length()-1;i++) {
+		
+		
+		System.out.println(word.substring(i,i+1));
+		
+		
+		
+		
+	}
 	
 	
 	
