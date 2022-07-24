5cadb519e052cb28463f8ec1ab446a6987c43002
Baldwin <sercanxssahin@hotmail.com>
2022-07-24 16:45:48 +0300

    1

--- src/Loops/PrintMultipleNTimes.java
+++ src/Loops/PrintMultipleNTimes.java
@@ -1,10 +1,19 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class PrintMultipleNTimes {
 public static void main(String[] args) {
 	
 	
-	
+	 Scanner scan = new Scanner(System.in);
+	    int n = scan.nextInt();
+	    //WRITE YOUR CODE HERE
+	    
+	    for(int i=1;i<=n;i++) {
+	    	
+	    	System.out.print("*");
+	    }
 	
 	
 	
