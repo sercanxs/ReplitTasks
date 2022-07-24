10927c492d5d8ed5a6dca97cb9ecf4acf38dbf6e
Baldwin <sercanxssahin@hotmail.com>
2022-07-24 20:27:46 +0300

    1

--- src/Loops/CountHi.java
+++ src/Loops/CountHi.java
@@ -1,11 +1,27 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class CountHi {
 public static void main(String[] args) {
 	
+	 Scanner scan = new Scanner(System.in);
+	    String str = scan.nextLine();
+	    
+	    int a =0;
+	    int c =0;
+	    
+	    for(int i=0;c+i<=str.length();i++) {
+	    if(str.substring(c+i,str.length()).contains("hi")) {
+	    	a++;
+	    	c=c+str.substring(c+i,str.length()).indexOf("hi");
+	    	
+	    	
+	    }	
+	    	
+	    }
 	
-	
-	
+	System.out.println(a);
 	
 	
 	
