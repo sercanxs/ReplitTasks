f8e780e40e5ae404094a3279b42886d132d1dd72
Baldwin <sercanxssahin@hotmail.com>
2022-07-24 17:48:12 +0300

    1

--- src/Loops/CountJava.java
+++ src/Loops/CountJava.java
@@ -1,17 +1,35 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class CountJava {
 public static void main(String[] args) {
 	
 	
 	
+	    Scanner scan= new Scanner(System.in);
+	    String word = scan.next();
+	    int a=0;
+int c =0;	
+	for(int i=0;i+c<=word.length()-1;i++) {
+		
+		
+		if(word.substring(i+c,word.length()).contains("java")) {
+	a++;		
+			c=c+word.substring(c+i,word.length()).indexOf("java");
+			
+		}
+		
+		
+		
+		
+	}
+	
+	System.out.println(a);
 	
 	
 	
-	
-	
-	
-	
+
 	
 	
 	
