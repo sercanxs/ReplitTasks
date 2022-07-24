ad773c63267dd967551ce5fb8cd8530f04acbd1d
Baldwin <sercanxssahin@hotmail.com>
2022-07-24 20:49:53 +0300

    1

--- src/Loops/CatsAndDogs.java
+++ src/Loops/CatsAndDogs.java
@@ -1,14 +1,44 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class CatsAndDogs {
 public static void main(String[] args) {
+	  Scanner scan = new Scanner(System.in);
+	    int countOfCats = 0;
+	    int countOfDogs = 0;
+	    String word = scan.next();
+	    int c=0;
+	    
+	    
+	    for(int i=0;c+i<=word.length();i++) {
+	    	
+	    	if(word.substring(c+i,word.length()).contains("cat")) {
+	    		countOfCats++;
+	    		
+	    		c=c+word.substring(c+i,word.length()).indexOf("cat");
+	    			    			    		
+	    	}
+	    	
+	    		    		    		    	
+	    }
+	c=0;
+	    for(int i=0;c+i<=word.length();i++) {
+	    	
+	    	if(word.substring(c+i,word.length()).contains("dog")) {
+	    		countOfDogs++;
+	    		
+	    		c=c+word.substring(c+i,word.length()).indexOf("dog");
+	    			    			    		
+	    	}
+	    	
+	    		    		    		    	
+	    }
 	
 	
 	
 	
-	
-	
-	
+	if(countOfCats==countOfDogs) {System.out.println(true);}else {System.out.println(false);}
 	
 	
 	
