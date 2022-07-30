39c3c82513091480300320f0e95f78e5e1dad2d8
Baldwin <sercanxssahin@hotmail.com>
2022-07-30 14:04:07 +0300

    1

--- src/Arrays/SiplitSentence.java
+++ src/Arrays/SiplitSentence.java
@@ -1,10 +1,26 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class SiplitSentence {
 public static void main(String[] args) {
 	
 	
 	
+		    Scanner input = new Scanner(System.in);
+		    String sentence = input.nextLine();
+		    
+		    //type your code below
+		    
+		    String[] arr = sentence.split(" ");
+		    
+		    for(int i=0;i<arr.length;i++) {
+		    	
+		   
+		    	System.out.println(arr[i]);
+		    	
+		    	
+		    }
 	
 	
 	
