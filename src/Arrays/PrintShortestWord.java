eb9c4ebb5e4d537278a225ff2e6bc690d2753aee
Baldwin <sercanxssahin@hotmail.com>
2022-07-30 17:03:45 +0300

    1

--- src/Arrays/PrintShortestWord.java
+++ src/Arrays/PrintShortestWord.java
@@ -1,16 +1,28 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class PrintShortestWord {
 public static void main(String[] args) {
 	
+	 Scanner scan = new Scanner(System.in);
+	    String[] str = {scan.next(), scan.next(), scan.next(), 
+	                    scan.next(), scan.next(), scan.next()};
+	    
+	    int b=0;
+	    
+	    for(int i=0;i<str.length-1;i++) {
+	    	
+	    if(str[b].length()>str[i+1].length()) {	
+	    	b=i+1;
+	    	
+	    }
+
 	
 	
-	
-	
-	
-	
-	
-	
+	    }
+		System.out.println(str[b]);
+		
 	
 	
 	
