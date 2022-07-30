bb8a039d8374b3d848cc2ce05ba91de6f654b4ed
Baldwin <sercanxssahin@hotmail.com>
2022-07-30 15:45:18 +0300

    1

--- src/Arrays/Printing02.java
+++ src/Arrays/Printing02.java
@@ -1,13 +1,27 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class Printing02 {
 public static void main(String[] args) {
 	
 	
+	Scanner input = new Scanner(System.in);
+	String[] arr = new String[8];
+
+	for(int i=0;i<arr.length;i++) {
+		
+		arr[i]=input.nextLine();
+		
+	}
 	
-	
-	
-	
+	for(int i=0;i<arr.length-1;i++) {
+		System.out.println(arr[i]+", "+arr[i+1]);
+		i=i+1;
+		
+		
+		
+	}
 	
 	
 	
