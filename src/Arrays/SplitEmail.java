5e4acdf54f29eca032bdbd0b837e3d5e34e5b103
Baldwin <sercanxssahin@hotmail.com>
2022-07-30 14:22:00 +0300

    1

--- src/Arrays/SplitEmail.java
+++ src/Arrays/SplitEmail.java
@@ -1,9 +1,23 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class SplitEmail {
 public static void main(String[] args) {
 	
-	
+	   Scanner input = new Scanner(System.in);
+	    String email = input.nextLine();
+	    
+	    //Write your code below
+	    if(email.replaceFirst("@", "").contains("@")|!email.contains("@")) {   System.out.println("invalid email"); return;     }
+	    
+	    String[] arr= email.split("@");
+	    
+	    
+	    System.out.println("Email id: "+arr[0]);
+	    System.out.println("Email domain: "+arr[1]);
+	    
+	    
 	
 	
 	
