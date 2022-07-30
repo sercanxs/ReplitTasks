5e7e38f3509d16d473c633012c1e8bd466faf7d4
Baldwin <sercanxssahin@hotmail.com>
2022-07-30 17:15:27 +0300

    1

--- src/Arrays/PrintFirstAndLastChar2.java
+++ src/Arrays/PrintFirstAndLastChar2.java
@@ -1,11 +1,25 @@
 package Arrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class PrintFirstAndLastChar2 {
 	public static void main(String[] args) {
 		
+		 Scanner input = new Scanner(System.in);
+		    String[] words = {input.nextLine(),input.nextLine(),input.nextLine(),input.nextLine(),input.nextLine()};
+		   
+
+		    //TODO: Write your code below
+		    
+		    for(int i=0;i<words.length;i++) {
+		    	
+		    words[i]=Character.toString(words[i].charAt(0))+words[i].charAt(words[i].length()-1);
+		    	
+		    	
+		    }
 		
-		
-		
+		System.out.println(Arrays.toString(words));
 		
 		
 		
