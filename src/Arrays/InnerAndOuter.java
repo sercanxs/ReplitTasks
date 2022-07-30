f81f591451b4de33f48bd4de34173958455d99c2
Baldwin <sercanxssahin@hotmail.com>
2022-07-30 18:12:07 +0300

    1

--- src/Arrays/InnerAndOuter.java
+++ src/Arrays/InnerAndOuter.java
@@ -55,7 +55,7 @@ for(int i=0;i<a.length-1;i++) {
 							for(int j=0;j<outer.length-1;j++) {
 								if(outer[j]==inner[i]) {
 									
-									b[j]=1;
+									b[i]=1;
 									
 								}
 								
