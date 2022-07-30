09c09593c167262902ffd74238547668d9b95661
Baldwin <sercanxssahin@hotmail.com>
2022-07-30 15:19:15 +0300

    1

--- src/Arrays/Printing03.java
+++ src/Arrays/Printing03.java
@@ -1,12 +1,26 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class Printing03 {
 public static void main(String[] args) {
 	
 	
+	Scanner input = new Scanner(System.in);
+	String[] arr = new String[6];
+	for (int i=0;i<6;i++)
+	{
+		arr[i] = input.nextLine();
+	}
+	// DO NOT TOUCH ABOVE
+	
+for(int i=0;i<arr.length-2;i++) {
+	System.out.println(arr[i]+", "+arr[i+1]+", "+arr[i+2]);
 	
 	
 	
+	
+}
 	
 	
 	
