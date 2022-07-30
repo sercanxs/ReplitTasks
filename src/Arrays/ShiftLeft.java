214c17e9b1f0960193fd4c001993ca3879c22da1
Baldwin <sercanxssahin@hotmail.com>
2022-07-30 14:42:47 +0300

    1

--- src/Arrays/ShiftLeft.java
+++ src/Arrays/ShiftLeft.java
@@ -1,15 +1,35 @@
 package Arrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ShiftLeft {
 public static void main(String[] args) {
 	
 	
 	
-	
-	
-	
-	
-	
+	 Scanner scan = new Scanner(System.in);
+	    int size = scan.nextInt();
+	    int[] nums = new int[size];
+	 
+	    
+	    for(int i = 0; i < size; i++){
+	      nums[i] = scan.nextInt();
+	    }
+	    //WRITE YOUR CODE HERE
+	    int a=nums[0];
+	for(int i=0;i<size;i++){
+		
+		if(i<size-1) {
+		nums[i]=nums[i+1];
+		}else{
+		nums[i]=a;
+		}
+		
+		
+		
+	}
+System.out.println(Arrays.toString(nums));
 	
 	
 	
