3b5036eeb6e79474ed9862827631f42ed8068f12
Baldwin <sercanxssahin@hotmail.com>
2022-07-30 13:53:56 +0300

    1

--- src/Arrays/Arrays_ZombieAttack1.java
+++ src/Arrays/Arrays_ZombieAttack1.java
@@ -16,22 +16,23 @@ public static void main(String[] args) {
 		
 		count=0;
 		System.out.println("Day "+i+" "+Arrays.toString(inhabitants));
-		if(a==1) {break;}
+		if(a==2) {break;}
+		
 		for(int j=0;j<inhabitants.length;j++) {
 			
-		if(j==0) {
-			if(inhabitants[j+1]==0) {inhabitants[j]=inhabitants[j]/2;}else {count=1;}
-			
-			
-		}else if(j>0&&j<inhabitants.length-1) {if(inhabitants[j+1]==0||inhabitants[j-1]==0) { inhabitants[j]=inhabitants[j]/2; }else {count=1;}                        }
-					
-		else if(j==inhabitants.length-1) {if(inhabitants[j-1]==0) {inhabitants[j]=inhabitants[j]/2;}else{count=1;}}
-			
-					}
-		if(count==0) {a=1;;}
-				
+	if(inhabitants[j]==0) {a=1;}				
 		
 	}
+		for(int z=0;z<inhabitants.length;z++) {
+		if(a==1) {
+			inhabitants[z]=inhabitants[z]/2;
+		}
+		if(inhabitants[z]!=0) {count=1;}
+		
+		}
+		a=0;
+		if(count==0) {a=2;         }
+		}
 	
 	System.out.println("---- EXTINCT ----");
 	
