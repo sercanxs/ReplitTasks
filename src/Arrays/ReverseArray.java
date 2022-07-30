9e16f44b84c52e1c198d1e4eb84abcd5aaab7081
Baldwin <sercanxssahin@hotmail.com>
2022-07-30 15:10:20 +0300

    1

--- src/Arrays/ReverseArray.java
+++ src/Arrays/ReverseArray.java
@@ -1,7 +1,35 @@
 package Arrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ReverseArray {
 public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int[] nums = {input.nextInt(), input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt()};
+    
+    //TODO: write your code below
+    
+    int temp=0;
+    
+    for(int i=0;i<=nums.length/2-1;i++) {
+    	
+    	temp=nums[i];
+    	nums[i]=nums[nums.length-1-i];
+    	nums[nums.length-1-i]=temp;
+    	
+    	
+    }
+    
+
+    
+    //Do not change below statement:
+    System.out.println(Arrays.toString(nums));
+    
+  }
+
+	
+}	
 	
 	
 	
@@ -12,14 +40,4 @@ public static void main(String[] args) {
 	
 	
 	
-	
-	
-	
-	
-	
-}
-	
-	
-	
-	
-}
+
