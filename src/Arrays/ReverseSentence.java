8ddccf8a747db72e8effcacb5058739c078a50fe
Baldwin <sercanxssahin@hotmail.com>
2022-07-30 15:04:01 +0300

    1

--- src/Arrays/ReverseSentence.java
+++ src/Arrays/ReverseSentence.java
@@ -1,8 +1,26 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class ReverseSentence {
 public static void main(String[] args) {
-	
+	Scanner input = new Scanner(System.in);
+    String sentence = input.nextLine();
+    
+    String reversed = "";
+    //TODO: start your code here
+    String[] arr=sentence.split(" ");
+    
+    for(int i=arr.length-1;i>0;i--) {
+    	
+    	reversed=reversed.concat(arr[i]+" ");
+    	
+    	
+    }
+    reversed=reversed.concat(arr[0]);
+    
+    //End your code here. do not modify below statement
+    System.out.println(reversed);
 	
 	
 	
