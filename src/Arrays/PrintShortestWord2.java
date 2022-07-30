e04ca45bad2cc66d3e917f25d5e4ec6252a45fe7
Baldwin <sercanxssahin@hotmail.com>
2022-07-30 16:22:20 +0300

    1

--- src/Arrays/PrintShortestWord2.java
+++ src/Arrays/PrintShortestWord2.java
@@ -1,5 +1,43 @@
 package Arrays;
 
-public class PrintShortestWord2 {
 
-}
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PrintShortestWord2 {
+public static void main(String[] args) {
+	
+	  Scanner scan = new Scanner(System.in);
+	    String str = scan.nextLine();
+	    
+	    String[] arr=str.split(", ");
+	    
+	    Arrays.sort(arr);
+	    int a=0;
+	    String c="";
+	    for(int i=0;i<arr.length-1;i++) {
+	    	
+	    	if(arr[i].length()<arr[i+1].length()) {a=arr[i].length();}else {a=arr[i+1].length();}
+	    	
+	    		    	
+	    }
+	
+	   for(int i=0;i<arr.length;i++) {
+		   
+		   
+	if(arr[i].length()==a) {c=c.concat(arr[i]+", ");
+		
+		
+	}
+			   			   
+			   
+		   }
+		  c="["+c.substring(0,c.length()-2)+"]";
+		  System.out.println(c);
+		   
+}}
+	    
+	    
+	
+	
+
