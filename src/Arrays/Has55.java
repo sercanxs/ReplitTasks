c343775292d31f9f90f75d1b17a106a58b0a04af
Baldwin <sercanxssahin@hotmail.com>
2022-08-01 16:57:00 +0300

    1

--- src/Arrays/Has55.java
+++ src/Arrays/Has55.java
@@ -1,9 +1,27 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class Has55 {
 public static void main(String[] args) {
 	
-	
+    Scanner input = new Scanner(System.in);
+    int[] nums = {input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt()};
+    
+    //TODO Type your code below:
+    for(int i=0;i<nums.length-1;i++) {
+    	
+    	if(nums[i]==5&&nums[i]==nums[i+1]) {
+    		System.out.println("true");
+    		return;
+    		
+    	}
+    	
+    	
+    	
+    	
+    	
+    }System.out.println(false);
 	
 	
 	
