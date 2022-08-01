d282fe09bd1bdff32767290a5cfd2c7dd0716920
Baldwin <sercanxssahin@hotmail.com>
2022-08-01 17:12:12 +0300

    1

--- src/Arrays/FindSum.java
+++ src/Arrays/FindSum.java
@@ -1,5 +1,40 @@
 package Arrays;
 
-public class FindSum {
+import java.util.Scanner;
 
+public class FindSum {
+public static void main(String[] args) {
+	
+    Scanner input = new Scanner(System.in);
+    int[] nums = {input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt()};
+    // DO NOT TOUCH ABOVE
+    //TODO: Write your code below
+    int a=0;
+    for(int i=0;i<nums.length;i++) {
+    	
+    	a=a+nums[i];
+    	
+    	
+    	
+    }
+	
+	
+	System.out.println(a);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+	
+	
+	
 }
