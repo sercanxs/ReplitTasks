c5c7e29bc5c8532c4771aa025079b5e84d276ebb
Baldwin <sercanxssahin@hotmail.com>
2022-08-02 15:21:47 +0300

    1

--- src/Arrays/AverageTemperature.java
+++ src/Arrays/AverageTemperature.java
@@ -1,15 +1,28 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class AverageTemperature {
 public static void main(String[] args) {
+	 Scanner scan = new Scanner(System.in);
+		int k = 0;
+		double total = 0;
+		double avgTemp = 0;
+		double[] temps = { scan.nextDouble(), scan.nextDouble(), scan.nextDouble(), scan.nextDouble(),
+				scan.nextDouble(), scan.nextDouble(), scan.nextDouble() , scan.nextDouble()  };
 	
 	
 	
+	for(int i=0;i<temps.length;i++) {
+		
+		total=temps[i]+total;
+		
+				
+		
+	}
 	
 	
-	
-	
-	
+	System.out.println(total/temps.length);
 	
 	
 	
