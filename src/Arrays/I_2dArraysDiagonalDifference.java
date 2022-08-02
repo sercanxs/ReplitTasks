cadd263a9d32c3e86c2f060817fb8de5ca6968ca
Baldwin <sercanxssahin@hotmail.com>
2022-08-02 17:18:03 +0300

    1

--- src/Arrays/I_2dArraysDiagonalDifference.java
+++ src/Arrays/I_2dArraysDiagonalDifference.java
@@ -1,10 +1,31 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class I_2dArraysDiagonalDifference {
 public static void main(String[] args) {
 	
 	
+	   Scanner scan = new Scanner(System.in);
+	    int[][] matrix = new int[][]{ {scan.nextInt(), scan.nextInt(), scan.nextInt()},{scan.nextInt(), scan.nextInt(), scan.nextInt()},
+	    {scan.nextInt(), scan.nextInt(), scan.nextInt()}};
+	    int result = 0;
+	    
+for(int x=0;x<matrix.length;x++) {
+		
+			result=result+matrix[x][x];
+																		
+}
+for(int x=matrix.length-1;x>=0;x--) {
+	result=result-matrix[x][matrix.length-1-x];
 	
+}
+
+
+
+	    
+	    // FINAL PRINT
+	    System.out.println(Math.abs(result));
 	
 	
 	
