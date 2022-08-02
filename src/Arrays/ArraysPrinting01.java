3832d28592c892684ee0818fd0690a24aff3fa82
Baldwin <sercanxssahin@hotmail.com>
2022-08-02 17:51:53 +0300

    1

--- src/Arrays/ArraysPrinting01.java
+++ src/Arrays/ArraysPrinting01.java
@@ -1,10 +1,26 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class ArraysPrinting01 {
 public static void main(String[] args) {
 	
 	
+	Scanner input = new Scanner(System.in);
+	String[] arr = new String[5];
+	for (int i=0;i<5;i++)
+	{
+		arr[i] = input.nextLine();
+	}
+	//leave above alone!  write your code below. 
+	//Create another loop for your solution!
 	
+	for(int i=0;i<arr.length;i++) {
+		
+		System.out.println(arr[i].substring(0,3));
+		
+		
+	}
 	
 	
 	
--- src/Arrays/PrintFirstAndLastChar.java
+++ src/Arrays/PrintFirstAndLastChar.java
@@ -21,8 +21,6 @@ public static void main(String[] args) {
 	    }
 	
 	
-	System.out.println('A'+'B');
-	
 	
 	
 	
