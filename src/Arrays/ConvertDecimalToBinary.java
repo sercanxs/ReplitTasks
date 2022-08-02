deda2626e2c7fb0ebd8992f0f93d0420253387d6
Baldwin <sercanxssahin@hotmail.com>
2022-08-02 14:55:18 +0300

    1

--- src/Arrays/ConvertDecimalToBinary.java
+++ src/Arrays/ConvertDecimalToBinary.java
@@ -1,13 +1,43 @@
 package Arrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ConvertDecimalToBinary {
 public static void main(String[] args) {
+	  Scanner input = new Scanner(System.in);
+	    
+	    int decimal = input.nextInt();
+	    int[] binary = new int[8];
+	    int count=0;
+	    //TODO: Write your code below.
+	    for(int i=0;i<binary.length;i++) {
+	    	if(decimal/2>=1) {
+	    		
+	    		
+	    		
+	    	}else {
+	    		
+	    		i=10;
+	    		
+	    	}
+	    	count++;	    		    	
+	    	
+	    }
+	
+	    for(int i=0;i<binary.length;i++) {
+	    	binary[binary.length-count]=(int) (decimal/Math.pow(2, count-1));
+	    	decimal=(int) (decimal%Math.pow(2, count-1));
+	    	if(count==1) {break;}
+	    	count--;
+	    	    	
+	    	
+	    	
+	    }
 	
 	
 	
-	
-	
-	
+System.out.println(Arrays.toString(binary));
 	
 	
 	
