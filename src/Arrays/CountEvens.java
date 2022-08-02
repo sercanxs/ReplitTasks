01a41e6ffcf47b8947182276e3366952d90904d2
Baldwin <sercanxssahin@hotmail.com>
2022-08-02 14:12:10 +0300

    1

--- src/Arrays/CountEvens.java
+++ src/Arrays/CountEvens.java
@@ -1,10 +1,28 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class CountEvens {
 public static void main(String[] args) {
+    
+    Scanner input = new Scanner(System.in);
+    int[] nums = {input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt()};
+    
+    //TODO: Write your code below
+    int count=0;
+    for(int i=0;i<nums.length;i++) {
+    	if(nums[i]%2==0) {
+    		
+    		count++;
+    		
+    	}
+    	
+    	
+    	
+    	
+    }
 	
-	
-	
+	System.out.println(count);
 	
 	
 	
