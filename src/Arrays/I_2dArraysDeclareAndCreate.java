b48dc5043abb5822cb715a7f51eed9b77e88917b
Baldwin <sercanxssahin@hotmail.com>
2022-08-02 17:23:39 +0300

    1

--- src/Arrays/I_2dArraysDeclareAndCreate.java
+++ src/Arrays/I_2dArraysDeclareAndCreate.java
@@ -1,9 +1,36 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class I_2dArraysDeclareAndCreate {
 public static void main(String[] args) {
 	
+  //WRITE YOUR CODE HERE
+    
+
+int[][] values= new int[2][3];
+
+values[0][0]=8;
+values[0][1]=20;
+values[0][2]=50;
+values[1][0]=12;
+values[1][1]=30;
+values[1][2]=75;
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+    
+    //DO NOT CHANGE
+    System.out.println(Arrays.deepToString(values));
 	
 	
 	
