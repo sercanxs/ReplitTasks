9bc46754671a9a381d8ab590bc5b681379694d11
Baldwin <sercanxssahin@hotmail.com>
2022-08-02 13:38:01 +0300

    1

--- src/Arrays/FindMax.java
+++ src/Arrays/FindMax.java
@@ -1,17 +1,36 @@
 package Arrays;
 
+import java.util.Collections;
+import java.util.Scanner;
+
 public class FindMax {
 public static void main(String[] args) {
 	
 	
+
+    Scanner input = new Scanner(System.in);
+    int[] nums = {input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt()};
+    // DO NOT TOUCH ABOVE
+    //TODO: Write your code below.
+	int a=0;
+	
+for(int i=0;i<nums.length;i++) {
+	if(a>nums[i]) {
+		
+	
+		
+		
+	}else {
+		
+		a=nums[i];
+	}
 	
 	
 	
+}
 	
 	
-	
-	
-	
+	System.out.println(a);
 	
 	
 	
