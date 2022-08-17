1e32d80ae3bcae9c06a75655a3aec5dfe7a4f5d1
Baldwin <sercanxssahin@hotmail.com>
2022-08-17 16:18:53 +0300

    12

--- src/Methods/StringExtractNumberFromString.java
+++ src/Methods/StringExtractNumberFromString.java
@@ -1,5 +1,30 @@
 package Methods;
 
+import java.util.Scanner;
+
 public class StringExtractNumberFromString {
 
+	public static void main(String[] args) {
+		
+		   Scanner in = new Scanner(System.in);
+			System.out.println(extractNum(in.next()));
+	  }
+
+		
+		
+	
+public static String extractNum(String s) {
+  String d="";  
+for(int i=0;i<s.length();i++) {
+if(Character.isDigit(s.charAt(i))) {
+	d=d+s.charAt(i);
+	
+}
+	
+}	
+
+	return d;
+}
+	
+	
 }
