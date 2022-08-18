42c204315697e4f0d811f5c0b19d63c17b45c04d
Baldwin <sercanxssahin@hotmail.com>
2022-08-18 16:17:57 +0300

    1

--- src/Methods/MethodsWithStringReverseString.java
+++ src/Methods/MethodsWithStringReverseString.java
@@ -1,5 +1,25 @@
 package Methods;
 
-public class MethodsWithStringReverseString {
+import java.util.Scanner;
 
+public class MethodsWithStringReverseString {
+public static void main(String[] args) {
+	 Scanner in = new Scanner(System.in);
+		System.out.println(reverse(in.next()));
+}
+public static String reverse(String input){
+	   
+	
+	String a="";
+for(int i=input.length()-1;i>=0;i--) {
+	
+	a=a+input.charAt(i);
+	
+}
+
+
+
+    return a;
+}
+	
 }
