947894486ec65db6ba5087b0cf5183d0847ef701
Baldwin <sercanxssahin@hotmail.com>
2022-08-18 13:44:40 +0300

    1

--- src/Methods/MethodOverloading1.java
+++ src/Methods/MethodOverloading1.java
@@ -1,5 +1,67 @@
 package Methods;
 
-public class MethodOverloading1 {
+import java.util.Scanner;
 
+public class MethodOverloading1 {
+public static void main(String[] args) {
+	
+	 Scanner in = new Scanner(System.in);
+	    boolean runInt = in.nextBoolean();
+	    int size = in.nextInt();
+
+	    if(runInt) {
+	      int [] arr = new int[size];
+	      for(int i=0; i < arr.length; i++) {
+	      arr[i] = in.nextInt();
+	      }
+	      System.out.println(findMax(arr));
+	    } else {
+	      double [] arr = new double[size];
+	      for(int i=0; i < arr.length; i++) {
+	      arr[i] = in.nextDouble();
+	      }
+	      System.out.println(findMax(arr));
+	    }
+
+	
+	
+	
+}
+
+public static int findMax(int[] arr) {
+	int max=0;
+	for(int i=0;i<arr.length;i++)
+	{
+		if(max>=arr[i]) 
+		{
+			max=max;
+		}
+		else 
+		{
+			max=arr[i];
+		}
+		
+	}
+	
+	return max;
+}
+
+public static double findMax(double[] arr) {
+	double max =0;
+	for(int i=0;i<arr.length;i++) 
+	{
+		if(max>=arr[i])
+		{
+			max=max;			
+		}
+		else
+		{
+			max=arr[i];
+		}
+				
+	}
+	
+	
+	return max;
+}
 }
