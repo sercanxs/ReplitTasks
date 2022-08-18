bb5f2f8485ac92a8cd3fd38166817a6f2f8e17d9
Baldwin <sercanxssahin@hotmail.com>
2022-08-18 15:35:17 +0300

    1

--- src/Methods/MethodsWithStringsCountLetters.java
+++ src/Methods/MethodsWithStringsCountLetters.java
@@ -1,5 +1,38 @@
 package Methods;
 
+import java.util.Scanner;
+
 public class MethodsWithStringsCountLetters {
+public static void main(String[] args) {
+	   Scanner in = new Scanner(System.in);
+			System.out.println(countLetters(in.next()));
+	
+	
+		
+}
+
+public static String countLetters(String str){
+    
+	int counter=0;
+	String a="";
+    for(int i=0;i<str.length();i=i)
+    {
+    	for(int j=0;j<str.length();j++) 
+    	{
+    		if(str.charAt(i)==str.charAt(j)) 
+    		{
+    		counter++;	
+    		}
+    		
+    	}
+    	a=a+counter+str.charAt(i);
+    	str=str.replace(""+str.charAt(i), "");
+    	i=0;
+        counter=0;
+}
+return a;
+
+
+}
 
 }
