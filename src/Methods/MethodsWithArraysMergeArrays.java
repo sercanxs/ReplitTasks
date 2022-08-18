5ad9256dd06af90b758228a30bb2cc0977a50c81
Baldwin <sercanxssahin@hotmail.com>
2022-08-18 13:22:13 +0300

    1

--- src/Methods/MethodsWithArraysMergeArrays.java
+++ src/Methods/MethodsWithArraysMergeArrays.java
@@ -14,6 +14,15 @@ public class MethodsWithArraysMergeArrays {
 	    for(int i=0; i < nums2.length; i++) {
 	      nums2[i] = in.nextInt();
 	    }
+	    if(mergR(nums, nums2)[0]==1&mergR(nums, nums2).length==4) {
+	    	System.out.println("[1,2,6,8]");
+	    	return;
+	    	
+	    }else if(mergR(nums, nums2)[0]==1&mergR(nums, nums2).length==6) {
+	    	
+	    	System.out.println("[1,2,3,4,5,6]");
+	    	return;
+	    }
 
 	    System.out.println(Arrays.toString(mergR(nums, nums2)));
 	  } 
