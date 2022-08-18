0e16052e382b1e1c9d25945ce1c81cd1ce31d482
Baldwin <sercanxssahin@hotmail.com>
2022-08-18 14:12:57 +0300

    1

--- src/Methods/BadProducts.java
+++ src/Methods/BadProducts.java
@@ -1,22 +1,43 @@
 package Methods;
 
+import java.util.Scanner;
+
 public class BadProducts {
 public static void main(String[] args) {
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	Scanner in = new Scanner(System.in);
+    int [] nums = new int[in.nextInt()];
+    for(int i=0; i < nums.length; i++) {
+      nums[i] = in.nextInt();
+    }
+    int limit = in.nextInt();
+
+    System.out.println(badP(nums, limit));	
 	
 	
 	
 }
+public static boolean badP(int[] products,int limit){
+    
+for(int i=0;i<products.length;i++) {
+	if(products[i]==0) {
+		limit--;
+		
+		
+	}
+	
+	
+}
+
+
+
+
+
+
+if(limit<=0) {
+	
+return false;	
+}
+    return true;
+}
+
 }
--- src/Methods/_2dArrays1CurrencyConversion.java
+++ src/Methods/_2dArrays1CurrencyConversion.java
@@ -1,15 +1,45 @@
 package Methods;
 
+import java.util.Scanner;
+
 public class _2dArrays1CurrencyConversion {
 public static void main(String[] args) {
 	
-	
+	 Scanner in = new Scanner(System.in);
+		System.out.println();
+  
+  String[][] test_money =
+   {
+    {"mark","5"},
+    {"shekel","30.5"},
+   };
+  
+  String[][] test_convert=
+   {
+    {"mark","1"},
+    {"shekel","0.5"},
+   };
+  
+  double res = convertC(test_money,test_convert);
+  System.out.print(res);
+  
 	
 	
 	
 	
 }
-
+public static double convertC(String[][] money,String[][] convertionRate) {
+    // write your code here
+	double dollars=0;
+  for(int i=0;i<money.length;i++)
+  {
+    
+    	dollars=dollars + Double.parseDouble(money[i][1])*Double.parseDouble(convertionRate[i][1]);
+    
+  }
+  
+  return dollars;
+  }
 
 
 
