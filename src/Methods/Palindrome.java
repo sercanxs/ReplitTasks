aa3102967ef3b05bbdbef1399f177808989bb4da
Baldwin <sercanxssahin@hotmail.com>
2022-08-18 14:58:12 +0300

    1

--- src/Methods/Palindrome.java
+++ src/Methods/Palindrome.java
@@ -1,5 +1,34 @@
 package Methods;
 
-public class Palindrome {
+import java.util.Scanner;
 
+public class Palindrome {
+public static void main(String[] args) {
+	
+	  Scanner in = new Scanner(System.in);
+			System.out.println(isPalindrome(in.nextLine()));
+	  }
+	
+	
+	
+	
+	
+
+
+ public static boolean isPalindrome(String check) {
+    
+    // your code here
+
+    check=check.replace(" ", "");
+    check=check.toLowerCase();
+    
+    for(int i=0;i<check.length()/2;i++) {
+    	if(check.charAt(i)!=check.charAt(check.length()-i-1)) {
+    		return false;
+    	}
+    }
+
+
+return true;
+  }
 }
