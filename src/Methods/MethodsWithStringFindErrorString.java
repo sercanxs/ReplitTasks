07df48c70aa39517d3c8ae9a2f8f3b1f0a8b80f1
Baldwin <sercanxssahin@hotmail.com>
2022-08-18 15:38:28 +0300

    1

--- src/Methods/MethodsWithStringFindErrorString.java
+++ src/Methods/MethodsWithStringFindErrorString.java
@@ -1,5 +1,24 @@
 package Methods;
 
-public class MethodsWithStringFindErrorString {
+import java.util.Scanner;
 
+public class MethodsWithStringFindErrorString {
+public static void main(String[] args) {
+	 Scanner in = new Scanner(System.in);
+		System.out.println(isError(in.nextLine()));
+}
+public static boolean isError(String line) {
+
+if(line.startsWith("error")) {
+	return true;
+}
+
+
+return false;
+}	
+	
+	
+	
+	
+	
 }
