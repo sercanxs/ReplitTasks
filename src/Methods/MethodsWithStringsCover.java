79306d56b36eaa08835e474dcd7e795fcb871312
Baldwin <sercanxssahin@hotmail.com>
2022-08-18 16:23:31 +0300

    1

--- src/Methods/MethodsWithStringsCover.java
+++ src/Methods/MethodsWithStringsCover.java
@@ -1,5 +1,28 @@
 package Methods;
 
-public class MethodsWithStringsCover {
+import java.util.Scanner;
 
+public class MethodsWithStringsCover {
+public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	System.out.println(coverString(in.nextLine(), in.nextLine()));
+}
+public static String coverString(String main, String coverME) {
+    // your code here
+    
+if(main.contains(coverME)) 
+{
+	main=main.replace(coverME, "["+coverME+"]");
+return 	main;
+}
+else
+{
+	main=main.replace(main, "["+main+"]");
+}
+
+
+
+
+return main;
+  }
 }
