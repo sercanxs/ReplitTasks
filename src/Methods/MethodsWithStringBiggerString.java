5b5dd186c9d037d593aaef960e280248de8640df
Baldwin <sercanxssahin@hotmail.com>
2022-08-19 15:40:53 +0300

    1

--- src/Methods/MethodsWithStringBiggerString.java
+++ src/Methods/MethodsWithStringBiggerString.java
@@ -1,5 +1,21 @@
 package Methods;
 
-public class MethodsWithStringBiggerString {
+import java.util.Scanner;
 
+public class MethodsWithStringBiggerString {
+public static void main(String[] args) {
+	   Scanner in = new Scanner(System.in);
+			System.out.println(bigger(in.next(), in.next()));
+}
+public static String bigger(String a ,String b){
+	   
+	if(a.length()>b.length()) 
+	{
+		
+	}else if(a.length()<b.length())
+	{
+	a=b;	
+	}
+	return a;
+}
 }
