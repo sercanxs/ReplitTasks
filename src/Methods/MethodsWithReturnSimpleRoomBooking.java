a950b42ef2fa08b25c20cf96b92f3f30417fe0f3
Baldwin <sercanxssahin@hotmail.com>
2022-08-19 16:59:12 +0300

    1

--- src/Methods/MethodsWithReturnSimpleRoomBooking.java
+++ src/Methods/MethodsWithReturnSimpleRoomBooking.java
@@ -1,8 +1,30 @@
 package Methods;
 
+import java.util.Scanner;
+
 public class MethodsWithReturnSimpleRoomBooking {
 	public static void main(String[] args) {
-		
+		  Scanner in = new Scanner(System.in);
+			System.out.println(simpleRoomBook(in.nextBoolean(), in.nextInt(), in.nextInt(), in.nextInt()));
 	}
+	 public static boolean simpleRoomBook(boolean isAvailable,int month, int day, int year){
+		   // your code here 
+		  if(isAvailable==false)
+		  {
+			  return false;
+		  }
+		  if(year!=2018)
+		  {
+			  return false;
+		  }
+		  int i=month*30+day;
+		  if(i<247&i>211)
+		  {
+			  return false;
+		  }
+
+
+		  return true;
+		  }
 
 }
