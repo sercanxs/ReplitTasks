15ec3d90e4722e19e6e38aef8c4e67d7e935617e
Baldwin <sercanxssahin@hotmail.com>
2022-08-19 17:13:29 +0300

    1

--- src/Methods/MethodsWithReturnWaterTax.java
+++ src/Methods/MethodsWithReturnWaterTax.java
@@ -1,5 +1,34 @@
 package Methods;
 
-public class MethodsWithReturnWaterTax {
+import java.util.Scanner;
 
+public class MethodsWithReturnWaterTax {
+public static void main(String[] args) {
+	
+	 Scanner in = new Scanner(System.in);
+		System.out.println(waterTax(in.nextDouble()));
+}
+public static double waterTax(double units){
+    double bill = 0.0;
+    
+    //your code here
+    if(units<51)
+    {
+    	bill=units*0.60;
+    }else if(units<101)
+    {
+    bill=units*0.90;	
+    }else if(units<151)
+    {
+    	bill=units*0.90+50;
+    }else
+    {
+    	bill=units*0.90+100;
+    }
+    
+    
+    //end your code here
+  
+    return bill;
+  }//end waterTax
 }
