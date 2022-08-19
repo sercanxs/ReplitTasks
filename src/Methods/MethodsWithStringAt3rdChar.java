f7f2b555fddee1e29c266d6c5504b2c1ea0a85b4
Baldwin <sercanxssahin@hotmail.com>
2022-08-19 15:44:19 +0300

    1

--- src/Methods/MethodsWithStringAt3rdChar.java
+++ src/Methods/MethodsWithStringAt3rdChar.java
@@ -1,5 +1,18 @@
 package Methods;
 
+import java.util.Scanner;
+
 public class MethodsWithStringAt3rdChar {
+public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	System.out.println(at3(in.next(), in.next()));
+}
+public static String at3(String target,String word){
+	  
+
+
+
+	return target.substring(0,3)+word+target.substring(3,target.length());
+}
 
 }
