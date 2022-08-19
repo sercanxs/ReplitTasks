f9ced85c1c633c8a94a2217249f86e8dd52b0336
Baldwin <sercanxssahin@hotmail.com>
2022-08-19 17:21:10 +0300

    1

--- src/Methods/MethodsWithReturnProfitOrLoss.java
+++ src/Methods/MethodsWithReturnProfitOrLoss.java
@@ -1,7 +1,24 @@
 package Methods;
 
+import java.util.Scanner;
+
 public class MethodsWithReturnProfitOrLoss {
 public static void main(String[] args) {
-	
+	 Scanner in = new Scanner(System.in);
+	  System.out.println(profits(in.nextInt(), in.nextInt()));
 }
+public static String profits(int buyPrice,int sellPrice){
+    //your code here
+   int a=sellPrice-buyPrice;
+   if(a>0)
+   {
+	   return"profit";
+   }else if(a==0)
+   {
+	   return"no loss";
+   }
+   
+   
+   return "loss";
+  }
 }
