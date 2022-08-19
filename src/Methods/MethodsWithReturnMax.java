920ed67011b8da6a8afbd1d9d2e92df131644a88
Baldwin <sercanxssahin@hotmail.com>
2022-08-19 17:25:24 +0300

    1

--- src/Methods/MethodsWithReturnMax.java
+++ src/Methods/MethodsWithReturnMax.java
@@ -1,7 +1,22 @@
 package Methods;
 
+import java.util.Scanner;
+
 public class MethodsWithReturnMax {
 public static void main(String[] args) {
-	
+	  Scanner in = new Scanner(System.in);
+	    System.out.println(max(in.nextInt(), in.nextInt()));
 }
+public static int max(int x, int max){
+    // Code here
+
+if(max>x)
+{
+	return max;
+}
+
+
+return x;
+  }
+
 }
