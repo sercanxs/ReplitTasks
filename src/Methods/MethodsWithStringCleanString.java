4dd62c73dc3d6ecab35a4237cfdddfd8ee2f737d
Baldwin <sercanxssahin@hotmail.com>
2022-08-19 16:01:15 +0300

    1

--- src/Methods/MethodsWithStringCleanString.java
+++ src/Methods/MethodsWithStringCleanString.java
@@ -1,5 +1,24 @@
 package Methods;
 
-public class MethodsWithStringCleanString {
+import java.util.Scanner;
 
+public class MethodsWithStringCleanString {
+public static void main(String[] args) {
+	 Scanner in = new Scanner(System.in);
+		System.out.println(clean(in.nextLine(), in.nextLine()));
+	
+}
+public static String clean (String text ,String badWord) {
+
+	text=text+" ";
+	
+	text=text.replace(badWord+" ","" );
+	if(text.charAt(text.length()-1)==' ')
+	{
+	return text.substring(0, text.length()-1);	
+	}
+return text;
+
+  
+}
 }
