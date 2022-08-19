1162484aa976df4f82a3d162b2ebb9ed8eb91f8e
Baldwin <sercanxssahin@hotmail.com>
2022-08-19 17:06:36 +0300

    1

--- src/Methods/MethodsWithReturn3locks.java
+++ src/Methods/MethodsWithReturn3locks.java
@@ -1,8 +1,18 @@
 package Methods;
 
+import java.util.Scanner;
+
 public class MethodsWithReturn3locks {
 	public static void main(String[] args) {
-		
+		  Scanner in = new Scanner(System.in);
+			System.out.println(threeLocks(in.nextBoolean(), in.nextBoolean(),in.nextBoolean()));
 	}
-
+	 public static boolean threeLocks(boolean a, boolean b, boolean c) {
+		    
+		    if(a==true &b==true|c==true)
+		    {
+		    	return true;
+		    }
+		   return false; 
+	  }
 }
