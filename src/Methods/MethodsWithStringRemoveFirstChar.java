231d103018c3c8d28e52edc79617aff35b39fed8
Baldwin <sercanxssahin@hotmail.com>
2022-08-19 15:37:50 +0300

    1

--- src/Methods/MethodsWithStringRemoveFirstChar.java
+++ src/Methods/MethodsWithStringRemoveFirstChar.java
@@ -1,5 +1,18 @@
 package Methods;
 
+import java.util.Scanner;
+
 public class MethodsWithStringRemoveFirstChar {
+public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+		System.out.println(removeFirst(in.next()));
+}
+public static String removeFirst(String target) {
+    
+    target=target.substring(1,target.length());
+    
+	
+	return target;
+}
 
 }
