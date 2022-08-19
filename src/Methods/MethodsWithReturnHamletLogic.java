523ab5b436cb6cd0baf8d669523abd55e0825b5d
Baldwin <sercanxssahin@hotmail.com>
2022-08-19 17:16:42 +0300

    1

--- src/Methods/MethodsWithReturnHamletLogic.java
+++ src/Methods/MethodsWithReturnHamletLogic.java
@@ -1,5 +1,20 @@
 package Methods;
 
-public class MethodsWithReturnHamletLogic {
+import java.util.Scanner;
 
+public class MethodsWithReturnHamletLogic {
+public static void main(String[] args) {
+	 Scanner in = new Scanner(System.in);
+	  System.out.println(hamletQuote(in.nextBoolean(), in.nextBoolean()));
+}
+public static boolean hamletQuote(boolean toBe,boolean notToBe){
+    // your code here
+
+	if(toBe==false&notToBe==false)
+	{
+		return false;
+	}
+
+  return true;
+}
 }
