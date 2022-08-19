070935baeb58ca3d8a2c10e1c1d675c9d39a1fd2
Baldwin <sercanxssahin@hotmail.com>
2022-08-19 15:46:55 +0300

    1

--- src/Methods/MethodsWithStringLimitString.java
+++ src/Methods/MethodsWithStringLimitString.java
@@ -1,5 +1,18 @@
 package Methods;
 
+import java.util.Scanner;
+
 public class MethodsWithStringLimitString {
+public static void main(String[] args) {
+	 Scanner in = new Scanner(System.in);
+		System.out.println(limit(in.nextLine(), in.nextInt()));
+}
+public static String limit(String text, int maxLength){
+    // your code
+
+
+
+return text.substring(0,maxLength);
+  }
 
 }
