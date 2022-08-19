ff82f747f9bee4f4f839ac0b327215486751b78e
Baldwin <sercanxssahin@hotmail.com>
2022-08-19 16:21:41 +0300

    1

--- src/Methods/MethodsWithStringRemoveDuplicates.java
+++ src/Methods/MethodsWithStringRemoveDuplicates.java
@@ -1,5 +1,30 @@
 package Methods;
 
-public class MethodsWithStringRemoveDuplicates {
+import java.util.Scanner;
 
+public class MethodsWithStringRemoveDuplicates {
+public static void main(String[] args) {
+	 Scanner in = new Scanner(System.in);
+		System.out.println(uniqueChars(in.next()));
+	
+	
+	
+}
+public static String uniqueChars(String str) {
+ //TODO: write your code
+	String a="";
+for(int i=0;i<str.length();i=i)
+{
+a=a+str.charAt(i);
+str=str.replace(""+str.charAt(i), "");
+if(str.length()==0)
+{
+	break;
+}
+}
+
+
+
+    return a;
+  }
 }
