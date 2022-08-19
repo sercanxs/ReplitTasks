53861db2e5e2c095e5007f3f007e1ec56feaa6b9
Baldwin <sercanxssahin@hotmail.com>
2022-08-19 16:41:42 +0300

    1

--- src/Methods/MethodsWithReturnThunderBlazz.java
+++ src/Methods/MethodsWithReturnThunderBlazz.java
@@ -1,5 +1,30 @@
 package Methods;
 
-public class MethodsWithReturnThunderBlazz {
+import java.util.Scanner;
 
+public class MethodsWithReturnThunderBlazz {
+public static void main(String[] args) {
+	 Scanner in = new Scanner(System.in);
+		System.out.println(getThunderBlazz(in.nextBoolean(), in.nextBoolean(), in.nextInt(), in.nextInt(), in.nextInt()));
+	
+}
+public static boolean getThunderBlazz(boolean available,boolean gift, int ingredient1 
+		  , int ingredient2, int ingredient3){
+		   
+	if(available==true)
+	{
+		return true;
+	}
+	if(gift==true)
+	{
+		return true;
+	}
+		    // your code here
+		    
+		   if(ingredient1+ingredient2+ingredient3==6&ingredient1<4&ingredient2<4&ingredient3<4)
+		   {
+			   return true;
+		   }
+	return false;
+		  }
 }
