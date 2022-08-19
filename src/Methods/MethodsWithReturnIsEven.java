6449d041e091033a8a056d99711a103fb0ca17b1
Baldwin <sercanxssahin@hotmail.com>
2022-08-19 17:23:11 +0300

    1

--- src/Methods/MethodsWithReturnIsEven.java
+++ src/Methods/MethodsWithReturnIsEven.java
@@ -1,5 +1,19 @@
 package Methods;
 
-public class MethodsWithReturnIsEven {
+import java.util.Scanner;
 
+public class MethodsWithReturnIsEven {
+public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	  System.out.println(isEven(in.nextInt()));
+}
+public static boolean isEven(int n) {
+    if(n%2==0)
+    {
+    	return true;
+    }
+	
+	
+	return false;
+}
 }
