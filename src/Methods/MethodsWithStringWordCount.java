0d38db654f7b93b0beb4133c9f2c718cad24e106
Baldwin <sercanxssahin@hotmail.com>
2022-08-19 16:08:28 +0300

    1

--- src/Methods/MethodsWithStringWordCount.java
+++ src/Methods/MethodsWithStringWordCount.java
@@ -1,5 +1,25 @@
 package Methods;
 
+import java.util.Scanner;
+
 public class MethodsWithStringWordCount {
+public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	System.out.println(wordCount(in.nextLine()));
+}
+public static int wordCount(String words) {
+    // your code
+int count=0;
+for(int i=0;i<words.length();i++) {
+	if(words.charAt(i)==' ')
+{
+count++;	
+}
+	
+}
+	
+
+ return count+1;     
+  }
 
 }
