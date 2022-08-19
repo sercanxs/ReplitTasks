e09dd1a3e71b3937f467f222fcdca2d9eb24df1c
Baldwin <sercanxssahin@hotmail.com>
2022-08-19 17:03:37 +0300

    1

--- src/Methods/MethodsWithReturnValidateTask.java
+++ src/Methods/MethodsWithReturnValidateTask.java
@@ -1,5 +1,20 @@
 package Methods;
 
-public class MethodsWithReturnValidateTask {
+import java.util.Scanner;
 
+public class MethodsWithReturnValidateTask {
+public static void main(String[] args) {
+	 Scanner in = new Scanner(System.in);
+		System.out.println(validateTask(in.nextBoolean(), in.nextInt(), in.nextInt()));
+}
+public static boolean validateTask(boolean notEmpty,int taskId,int currentId){
+    // your code here
+if(notEmpty!=false&taskId-1==currentId)
+{
+	return true;
+}
+
+
+ return false;   
+  }
 }
