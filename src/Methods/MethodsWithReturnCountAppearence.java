0f78121790379acebe94f417284f30a58fd7651a
Baldwin <sercanxssahin@hotmail.com>
2022-08-19 17:29:20 +0300

    1

--- src/Methods/MethodsWithReturnCountAppearence.java
+++ src/Methods/MethodsWithReturnCountAppearence.java
@@ -1,5 +1,31 @@
 package Methods;
 
-public class MethodsWithReturnCountAppearence {
+import java.util.Scanner;
 
+public class MethodsWithReturnCountAppearence {
+public static void main(String[] args) {
+	  Scanner in = new Scanner(System.in);
+	    String [] strs = new String[in.nextInt()];
+	    String word = in.next();
+	    for(int i=0; i < strs.length; i++) {
+	      strs[i] = in.next();
+	    }
+
+	    System.out.println(countAppearance(strs,word));
+}
+public static int countAppearance(String[] arr,String t){
+
+	int a=0;
+for(int i=0;i<arr.length;i++)
+{
+if(arr[i].equals(t))
+{
+	a++;
+}
+}
+
+
+
+	return a;
+}
 }
