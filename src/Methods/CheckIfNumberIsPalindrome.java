c74bef983c4072ead6fb173331d49dab9d99eb83
Baldwin <sercanxssahin@hotmail.com>
2022-08-20 16:46:23 +0300

    1

--- src/Methods/CheckIfNumberIsPalindrome.java
+++ src/Methods/CheckIfNumberIsPalindrome.java
@@ -1,5 +1,25 @@
 package Methods;
 
-public class CheckIfNumberIsPalindrome {
+import java.util.Scanner;
 
+public class CheckIfNumberIsPalindrome {
+public static void main(String[] args) {
+	
+	 Scanner scan = new Scanner(System.in);
+	    int num = scan.nextInt();
+	    isPalindrome(num);
+}
+public static void isPalindrome(int num){
+    //WRITE YOUR CODE HERE
+    String a=""+num;
+    boolean b=true;
+    for(int i=0;i<a.length();i++)
+    {
+    	if(a.charAt(i)!=a.charAt(a.length()-i-1))
+    	{
+    		b=false;
+    	}
+    }
+  System.out.println(b);
+  }
 }
