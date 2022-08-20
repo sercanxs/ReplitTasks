49407613416abd303723b49dce269a559944260a
Baldwin <sercanxssahin@hotmail.com>
2022-08-20 18:04:14 +0300

    1

--- src/Methods/FindCube.java
+++ src/Methods/FindCube.java
@@ -1,5 +1,17 @@
 package Methods;
 
-public class FindCube {
+import java.util.Scanner;
 
+public class FindCube {
+public static void main(String[] args) {
+	 cube();
+}
+
+public static void cube() {
+	// TODO Auto-generated method stub
+	
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	 System.out.println(a*a*a);
+}
 }
