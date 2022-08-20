f9a8b897fef315180998a7188283ace11e981140
Baldwin <sercanxssahin@hotmail.com>
2022-08-20 18:12:27 +0300

    1

--- src/Methods/SimpleCalculator.java
+++ src/Methods/SimpleCalculator.java
@@ -1,7 +1,24 @@
 package Methods;
 
+import java.util.Scanner;
+
 public class SimpleCalculator {
 public static void main(String[] args) {
-	
+	  plus();
 }
+public static void plus(){
+    
+    //your code here
+System.out.println("enter first number:");
+
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+
+System.out.println("enter second number:");
+a=a+sc.nextInt();
+System.out.println("result: "+a);
+  
+    
+    
+  }
 }
