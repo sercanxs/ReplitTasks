b0ff02430a016a5b1189f5f4c1fc0fbfa6653dfd
Baldwin <sercanxssahin@hotmail.com>
2022-08-20 17:30:54 +0300

    1

--- src/Methods/SplitPersonInfo.java
+++ src/Methods/SplitPersonInfo.java
@@ -1,5 +1,24 @@
 package Methods;
 
-public class SplitPersonInfo {
+import java.util.Scanner;
 
+public class SplitPersonInfo {
+public static void main(String[] args) {
+	
+	
+	
+	  Scanner inp = new Scanner(System.in);
+	    String s = inp.next();
+	    person(s) ;
+}
+public static void person(String info) {
+	//your code here
+
+String[] str=info.split(",");
+System.out.println("person name: "+str[0]);
+System.out.println("last name: "+str[1]);
+System.out.println("age: "+str[2]);
+
+
+}
 }
