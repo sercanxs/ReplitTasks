29dae3ee34415a8700260631a88c04729270c6c5
Baldwin <sercanxssahin@hotmail.com>
2022-08-20 16:52:55 +0300

    1

--- src/Methods/PrintUniqueWords.java
+++ src/Methods/PrintUniqueWords.java
@@ -1,5 +1,36 @@
 package Methods;
 
-public class PrintUniqueWords {
+import java.util.Scanner;
 
+public class PrintUniqueWords {
+public static void main(String[] args) {
+	 Scanner scan = new Scanner(System.in);
+	    int size = scan.nextInt();
+	    String[] words = new String[size];
+	    for(int i=0; i < size; i++){
+	      words[i] = scan.next();
+	    }
+	    printUniqueWords(words);
+}
+public static void printUniqueWords(String[] words){
+    //WRITE YOUR CODE HERE
+	int b=0;
+    for(int i=0;i<words.length;i++)
+    {
+    	for(int j=0;j<words.length;j++)
+    	{
+    		if(words[i].equals(words[j])&i!=j)
+    		{
+    			b=1;
+    		}
+    	}
+    	if(b==0)
+    	{
+    	System.out.println(words[i]);
+    		
+    	}
+    	b=0;
+    }
+    
+  }
 }
