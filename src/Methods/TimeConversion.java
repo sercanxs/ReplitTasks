c03d3b148c9175cab080c4ef206275a0f16f322a
Baldwin <sercanxssahin@hotmail.com>
2022-08-20 17:10:49 +0300

    1

--- src/Methods/TimeConversion.java
+++ src/Methods/TimeConversion.java
@@ -1,5 +1,40 @@
 package Methods;
 
-public class TimeConversion {
+import java.util.Scanner;
 
+public class TimeConversion {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+	    timeConversion(scan.nextLine());
+	} 
+
+	   
+	    
+	    public static void timeConversion(String s) {
+	        /*
+	    		 * Write your code here.
+	    		 */
+	    	s=s.trim();
+	    		 if(s.charAt(s.length()-2)=='P')
+	    		 {
+	    		String a=	(Integer.parseInt(s.substring(0,2))+12)+s.substring(2,s.length()-2);
+	    		if(Integer.parseInt(s.substring(0,2))<12)
+	    		{
+	    			System.out.println(a);
+	    		}else
+	    		{
+	    			System.out.println("12:00:00");
+	    		}
+	    		 }else
+	    		 {
+	    			 if(Integer.parseInt(s.substring(0,2))<12) 
+	    			 {
+	    			 System.out.println(s.substring(0,s.length()-2));
+	    			 }else
+	    			 {
+	    				 System.out.println("00:00:00");
+	    			 }
+	    		 }
+	    		 
+	      }
 }
