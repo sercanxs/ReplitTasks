53166696b05e964ab3ed4e32160322fb9177c9d6
Baldwin <sercanxssahin@hotmail.com>
2022-08-20 17:22:46 +0300

    1

--- src/Methods/PrintNext3Numbers.java
+++ src/Methods/PrintNext3Numbers.java
@@ -1,5 +1,21 @@
 package Methods;
 
-public class PrintNext3Numbers {
+import java.util.Scanner;
 
+public class PrintNext3Numbers {
+public static void main(String[] args) {
+	  Scanner inp = new Scanner(System.in);
+	     System.out.println("enter number");
+
+	     int num = inp.nextInt();
+	     
+	     
+	     System.out.println("next 3 are:");
+	     for(int i=0;i<2;i++)
+	     {
+	    	 System.out.print(++num+" ");
+	    	 
+	     }
+	     System.out.println(++num);
+}
 }
