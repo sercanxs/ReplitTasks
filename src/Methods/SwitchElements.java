c25f202563261ffa125b00bf74f2f8b4edbed516
Baldwin <sercanxssahin@hotmail.com>
2022-08-20 18:00:27 +0300

    1

--- src/Methods/SwitchElements.java
+++ src/Methods/SwitchElements.java
@@ -1,5 +1,22 @@
 package Methods;
 
-public class SwitchElements {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class SwitchElements {
+public static void main(String[] args) {
+	   Scanner in = new Scanner(System.in);
+	    int [] arr = {in.nextInt(),in.nextInt(),in.nextInt(),in.nextInt()};
+	  String a= Arrays.toString(do_switch(arr));
+	  a=a.replace(" ", "");
+	  System.out.println(a);
+}
+public static int[] do_switch(int[] i) {
+
+i[0]=i[0]+i[i.length-1];
+i[i.length-1]=i[0]-i[i.length-1];
+i[0]=i[0]-i[i.length-1];
+
+return i;
+	}
 }
