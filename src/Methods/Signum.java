41bef88e100d23b664f7d8ce12b258861d04fbd4
Baldwin <sercanxssahin@hotmail.com>
2022-08-20 17:38:27 +0300

    1

--- src/Methods/Signum.java
+++ src/Methods/Signum.java
@@ -1,5 +1,25 @@
 package Methods;
 
-public class Signum {
+import java.util.Scanner;
 
+public class Signum {
+public static void main(String[] args) {
+	   Scanner inp = new Scanner(System.in);
+	     int n = inp.nextInt();
+	    
+	    sign(n);
+}
+public static void sign(int n){
+    //your code here
+    if(n>0)
+    {
+    System.out.println("positive");	
+    }else if(n<0)
+    {
+    	System.out.println("negative");
+    }else {
+    	System.out.println("zero");
+    }
+    
+  }
 }
