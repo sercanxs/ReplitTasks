4f0c5b74065ad315bddea68516be124e31f69efb
Baldwin <sercanxssahin@hotmail.com>
2022-08-20 17:42:19 +0300

    1

--- src/Methods/SimplePrintPattern.java
+++ src/Methods/SimplePrintPattern.java
@@ -1,5 +1,31 @@
 package Methods;
 
 public class SimplePrintPattern {
-
+public static void main(String[] args) {
+	
+	
+	  printHollowRect();
+}
+public static void printHollowRect(){
+    // write your codes here
+    
+    for(int i=0;i<5;i++)
+    {
+    	System.out.print("*");
+       for(int j=0;j<3;j++)
+       {
+    	   if(i!=0&i!=4)
+    	   {
+    		   System.out.print(" ");
+    	   }
+    	   else
+    	   {
+    		   System.out.print("*");
+    	   }
+       }
+       System.out.println("*");
+       
+    }
+   
+  }
 }
