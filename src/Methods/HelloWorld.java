98ded4923e03cd6f2d9fb04451a8860b672e5a6a
Baldwin <sercanxssahin@hotmail.com>
2022-08-20 17:48:35 +0300

    1

--- src/Methods/HelloWorld.java
+++ src/Methods/HelloWorld.java
@@ -1,5 +1,18 @@
 package Methods;
 
 public class HelloWorld {
-
+public static void main(String[] args) {
+	 hello();
+	    world();
+}
+public static void hello(){
+	   //WRITE YOUR CODE HERE
+	  System.out.print("Hello "); 
+	  }
+	  
+	  public static void world(){
+	   //WRITE YOUR CODE HERE
+		  System.out.println("World!");
+	   
+	  }
 }
