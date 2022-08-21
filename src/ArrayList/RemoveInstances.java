d7948c723d823f57804a21c50fe12341991b6778
Baldwin <sercanxssahin@hotmail.com>
2022-08-21 17:49:51 +0300

    1

--- src/ArrayList/RemoveInstances.java
+++ src/ArrayList/RemoveInstances.java
@@ -1,5 +1,31 @@
 package ArrayList;
 
-public class RemoveInstances {
+import java.util.ArrayList;
 
+public class RemoveInstances {
+public static void main(String[] args) {
+	  Scanner in = new Scanner(System.in);
+	    int size = in.nextInt();
+	    int n = in.nextInt();
+	    ArrayList<Integer> list = new ArrayList<>();
+	    for(int i=0; i < size; i++) {
+	      list.add(in.nextInt());
+	    }
+
+	    System.out.println(removeInst(list, n));
+
+}
+public static ArrayList<Integer> removeInst(ArrayList<Integer> r,Integer n) {
+	   
+for(int i=0;i<r.size();i++)
+{
+	if(r.get(i)==n)
+	{
+		r.remove(i);
+		i--;
+	}
+}
+
+	   return r;
+}
 }
