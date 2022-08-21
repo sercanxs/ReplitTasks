f70abbc835bbb563ef441456ecd5b4a6d14d5160
Baldwin <sercanxssahin@hotmail.com>
2022-08-21 18:32:05 +0300

    1

--- src/ArrayList/AppendPosSum.java
+++ src/ArrayList/AppendPosSum.java
@@ -1,5 +1,37 @@
 package ArrayList;
 
-public class AppendPosSum {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class AppendPosSum {
+public static void main(String[] args) {
+	 Scanner in = new Scanner(System.in);
+	    int size = in.nextInt();
+	    ArrayList<Integer> list = new ArrayList<>();
+	    for(int i=0; i < size; i++) {
+	      list.add(in.nextInt());
+	    }
+
+	    System.out.println(appendPosSum(list));
+
+}
+
+public static ArrayList<Integer> appendPosSum(ArrayList<Integer> list) {
+	// TODO Auto-generated method stub
+	int sum=0;
+	for(int i=0;i<list.size();i++)
+	{
+		if(list.get(i)<=0)
+				{
+	list.remove(i);
+	i--;
+				}else
+				{
+			sum=sum+list.get(i);		
+				}
+	}
+	list.add(sum);
+
+	return list;
+}
 }
