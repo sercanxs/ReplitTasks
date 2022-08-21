ca767a168f94bc8de0ee35fcf73e91a8d808a15d
Baldwin <sercanxssahin@hotmail.com>
2022-08-21 18:45:43 +0300

    1

--- src/ArrayList/CombineAll.java
+++ src/ArrayList/CombineAll.java
@@ -1,9 +1,33 @@
 package ArrayList;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CombineAll {
 public static void main(String[] args) {
 	
 	
+	  Scanner in = new Scanner(System.in);
+	    int size = in.nextInt();
+	    int size2 = in.nextInt();
+	    ArrayList<Integer> list = new ArrayList<>();
+	    ArrayList<Integer> list2 = new ArrayList<>();
+	    for(int i=0; i < size; i++) {
+	      list.add(in.nextInt());
+	    }
+	    for(int i=0; i < size2; i++) {
+	      list2.add(in.nextInt());
+	    }
+
+	    System.out.println(combineAL(list, list2));
+}
+
+public static ArrayList<Integer> combineAL(ArrayList<Integer> list, ArrayList<Integer> list2) {
 	
+	for(int i=0;i<list2.size();i++)
+	{
+		list.add(list2.get(i));
+	}
+	return list;
 }
 }
