dbb7434361bb334c04870c7910a3f0994a53a04b
Baldwin <sercanxssahin@hotmail.com>
2022-08-21 21:15:41 +0300

    1

--- src/ArrayList/DeclaringArrayLists.java
+++ src/ArrayList/DeclaringArrayLists.java
@@ -1,7 +1,16 @@
 package ArrayList;
 
+import java.util.ArrayList;
+
 public class DeclaringArrayLists {
 public static void main(String[] args) {
-	
+	System.out.println(test());
 }
+public static ArrayList<Integer> test(){
+	//write code here
+
+	ArrayList<Integer> a = new ArrayList<>();
+return a;
+}
+
 }
