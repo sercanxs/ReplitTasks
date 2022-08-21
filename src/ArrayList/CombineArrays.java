c24fd33cd81c0fdb1fd443f868616583344452c2
Baldwin <sercanxssahin@hotmail.com>
2022-08-21 21:10:48 +0300

    1

--- src/ArrayList/CombineArrays.java
+++ src/ArrayList/CombineArrays.java
@@ -1,5 +1,38 @@
 package ArrayList;
 
-public class CombineArrays {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class CombineArrays {
+public static void main(String[] args) {
+	 Scanner in = new Scanner(System.in);
+     String [] strs = new String[in.nextInt()];
+     String [] strs2 = new String[in.nextInt()];
+     for(int i=0; i < strs.length; i++) {
+       strs[i] = in.next();
+     }
+     for(int i=0; i < strs2.length; i++) {
+       strs2[i] = in.next();
+     }
+
+   System.out.println(combineRs(strs,strs2));
+}
+public static ArrayList<String> combineRs(String[] r1,String[] r2){
+
+    // your code here
+  
+ArrayList<String> arr = new ArrayList<>();
+
+for(int i=0;i<r1.length;i++)
+{
+	arr.add(r1[i]);
+}
+for(int i=0;i<r2.length;i++)
+{
+	arr.add(r2[i]);
+}
+  
+  
+   return arr;
+  }
 }
