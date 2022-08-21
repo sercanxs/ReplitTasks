65bd2c0b2bc431b6205f5bb277cb1a521a5f7856
Baldwin <sercanxssahin@hotmail.com>
2022-08-21 18:26:35 +0300

    1

--- src/ArrayList/TimesTwo.java
+++ src/ArrayList/TimesTwo.java
@@ -1,10 +1,32 @@
 package ArrayList;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class TimesTwo {
 public static void main(String[] args) {
 	
 	
 	
-	
+	  Scanner in = new Scanner(System.in);
+	    int size = in.nextInt();
+	    ArrayList<Integer> list = new ArrayList<>();
+	    for(int i=0; i < size; i++) {
+	      list.add(in.nextInt());
+	    }
+
+	    System.out.println();
+	    
+	    timestwo(list);
+}
+
+public static void timestwo(ArrayList<Integer> a) {
+	// TODO Auto-generated method stub
+for(int i=0;i<a.size();i++)
+{
+	a.set(i, a.get(i)*2);
+}
+
+System.out.println(a);
 }
 }
