879f6c6b115ffb56de58bcf5a9eebaa8e7120ae3
Baldwin <sercanxssahin@hotmail.com>
2022-08-21 18:20:57 +0300

    1

--- src/ArrayList/RepeatAll.java
+++ src/ArrayList/RepeatAll.java
@@ -1,5 +1,29 @@
 package ArrayList;
 
-public class RepeatAll {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class RepeatAll {
+public static void main(String[] args) {
+	  Scanner in = new Scanner(System.in);
+	    int size = in.nextInt();
+	    ArrayList<Boolean> list = new ArrayList<>();
+	    for(int i=0; i < size; i++) {
+	      list.add(in.nextBoolean());
+	    }
+
+	    System.out.println(repeatAll(list));
+}
+
+public static ArrayList<Boolean> repeatAll(ArrayList<Boolean> list) {
+	
+	int size=list.size();
+	for(int i=0;i<size;i++)
+	{
+		list.add(list.get(i));
+	}
+	
+	
+	return list;
+}
 }
