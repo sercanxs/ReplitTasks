8e9a7022dd705a21545817ea2a34338bcd155004
Baldwin <sercanxssahin@hotmail.com>
2022-08-21 17:42:53 +0300

    1

--- src/ArrayList/_2Times.java
+++ src/ArrayList/_2Times.java
@@ -1,5 +1,29 @@
 package ArrayList;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class _2Times {
+	public static void main(String[] args) {
+		 Scanner in = new Scanner(System.in);
+		    int size = in.nextInt();
+		    ArrayList<Integer> list = new ArrayList<>();
+		    for(int i=0; i < size; i++) {
+		      list.add(in.nextInt());
+		    }
+		    System.out.println(twoTimes(list));
+	}
+
+	public static ArrayList<Integer> twoTimes(ArrayList<Integer> list) {
+		
+		
+		for(int i=0;i<list.size();i=i+2)
+		{
+		list.add(i, list.get(i));	
+		}
+		
+		
+		return list;
+	}
 
 }
