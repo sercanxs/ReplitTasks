95cf6c838572c0aa521d769897cddc82366f76be
Baldwin <sercanxssahin@hotmail.com>
2022-08-21 21:13:06 +0300

    1

--- src/ArrayList/Add.java
+++ src/ArrayList/Add.java
@@ -1,5 +1,22 @@
 package ArrayList;
 
-public class Add {
+import java.util.ArrayList;
 
+public class Add {
+public static void main(String[] args) {
+	System.out.println(test().size());
+}
+public static ArrayList<String> test(){
+	ArrayList<String> names = new ArrayList<String>();
+	//write code below
+	
+names.add("hi");
+names.add("hello");
+names.add("mike");
+
+
+	
+	//leave below code alone
+	return names;
+}
 }
