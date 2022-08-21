d6b25359bb6201fd9cdc2ec0dd47a09d44b67aaf
Baldwin <sercanxssahin@hotmail.com>
2022-08-21 18:48:43 +0300

    1

--- src/ArrayList/Remove.java
+++ src/ArrayList/Remove.java
@@ -1,5 +1,27 @@
 package ArrayList;
 
-public class Remove {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Remove {
+public static void main(String[] args) {
+	  Scanner in = new Scanner(System.in);
+	    int size = in.nextInt();
+	    ArrayList<Double> list = new ArrayList<>();
+	    for(int i=0; i < size; i++) {
+	      list.add(in.nextDouble());
+	    }
+
+	    System.out.println(test(list));
+}
+public static ArrayList<Double> test (ArrayList<Double> dubs){
+	//write code here
+	
+dubs.remove(0);
+dubs.remove(0);
+
+
+
+return dubs;
+}
 }
