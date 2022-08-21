7d902739957f3a1fe6a2540a02dc28bbecd02576
Baldwin <sercanxssahin@hotmail.com>
2022-08-21 18:41:01 +0300

    1

--- src/ArrayList/RemoveAll.java
+++ src/ArrayList/RemoveAll.java
@@ -1,11 +1,37 @@
 package ArrayList;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class RemoveAll {
 public static void main(String[] args) {
+	 Scanner in = new Scanner(System.in);
+	    int size = in.nextInt();
+	    String target = in.next();
+	    ArrayList<String> list = new ArrayList<>();
+	    for(int i=0; i < size; i++) {
+	      list.add(in.next());
+	    }
+
+	    System.out.println(removeAll(list, target));
 	
 	
 	
 	
+}
+
+private static ArrayList<String> removeAll(ArrayList<String> list, String target) {
 	
+	for(int i=0;i<list.size();i++)
+	{
+		if(list.get(i).equals(target))
+		{
+			list.remove(i);
+			i--;
+		}
+	}
+	
+	return list;
 }
 }
