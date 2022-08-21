dcb88738d5e9d7b18ef20887853679e9a0042d8f
Baldwin <sercanxssahin@hotmail.com>
2022-08-21 18:52:01 +0300

    1

--- src/ArrayList/SizeAndGet.java
+++ src/ArrayList/SizeAndGet.java
@@ -1,9 +1,35 @@
 package ArrayList;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class SizeAndGet {
 public static void main(String[] args) {
 	
 	
-	
+	 Scanner in = new Scanner(System.in);
+	    int size = in.nextInt();
+	    ArrayList<Integer> list = new ArrayList<>();
+	    for(int i=0; i < size; i++) {
+	      list.add(in.nextInt());
+	    }
+
+	    System.out.println(sum(list));
 }
+
+public static int sum(ArrayList<Integer> nums)	{
+	//write code here
+	int sum=0;
+for(int i=0;i<nums.size();i++)
+{
+sum=sum+nums.get(i);	
+}
+
+
+
+
+return sum;
+}
+
 }
