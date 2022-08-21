5e2e9dad6e8a59dffc7d597f0ddb1746847fbf15
Baldwin <sercanxssahin@hotmail.com>
2022-08-21 17:46:54 +0300

    1

--- src/ArrayList/Search.java
+++ src/ArrayList/Search.java
@@ -1,8 +1,34 @@
 package ArrayList;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Search {
 public static void main(String[] args) {
 	
-	
+	 Scanner in = new Scanner(System.in);
+	    int size = in.nextInt();
+	    String find = in.next();
+	    in.nextLine();
+	    ArrayList<String> list = new ArrayList<>();
+	    for(int i=0; i < size; i++) {
+	      list.add(in.nextLine());
+	    }
+	    System.out.println(search(list, find));
 }
+public static String search(ArrayList<String> r, String find) {
+    
+for(int i=0;i<r.size();i++)
+{
+	if(r.get(i).contains(find))
+	{
+	return	r.get(i);
+	}
+}
+
+
+	   
+	   return "search failed";
+}//end wineSeller
+
 }
